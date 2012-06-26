59b08d4055ac186f5514e7d7d36fdd1f8b092224
jeff <jeff@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-06-26 12:41:21 +0000

    Committed Joyce's Changes
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/GermplasmStudyBrowser@244 4ebb2e00-0138-4608-bc6e-edf3948d4863

---
+++ src/main/java/org/generationcp/browser/i18n/ui/I18NHorizontalLayout.java
@@ -0,0 +1,59 @@
+package org.generationcp.browser.i18n.ui;
+
+import java.util.Locale;
+
+import com.github.peholmst.i18n4vaadin.I18N;
+import com.github.peholmst.i18n4vaadin.I18NComponent;
+import com.github.peholmst.i18n4vaadin.I18NListener;
+import com.github.peholmst.i18n4vaadin.support.I18NComponentSupport;
+import com.vaadin.ui.HorizontalLayout;
+
+
+public class I18NHorizontalLayout extends HorizontalLayout implements
+		I18NComponent, I18NListener {
+
+	private static final long serialVersionUID = 472093734197217L;
+	private final I18NComponentSupport support;
+
+	public I18NHorizontalLayout(I18N i18n) {
+		support = new I18NComponentSupport(this);
+		support.setI18N(i18n);
+	}
+	
+	@Override
+	public void setI18N(I18N i18n) {
+		support.setI18N(i18n);
+	}
+
+	@Override
+	public I18N getI18N() {
+		return support.getI18N();
+	}
+
+	@Override
+	public void attach() {
+		super.attach();
+		//getI18N().addListener(this);
+		updateLabels();
+	}
+
+	@Override
+	public void detach() {
+		//getI18N().removeListener(this);
+		super.detach();
+	}
+
+	@Override
+	public void localeChanged(I18N sender, Locale oldLocale, Locale newLocale) {
+		updateLabels();
+	}
+
+	/** 
+	 * Change the label of the components based on the selected Locale
+	 * Override if there's a need to change the labels on the fly
+	 */
+	protected void updateLabels() {
+	}
+
+
+}
