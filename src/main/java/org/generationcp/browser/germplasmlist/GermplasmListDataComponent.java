76ef70d3f14feac0473a46461cd0a2b040ccd10f
Macky <Macky@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2012-08-22 13:27:23 +0000

    Code Changes for: GCP-68 - Germplasm List Data Browser - added internationalization messages
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/GermplasmStudyBrowser@612 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/browser/germplasmlist/GermplasmListDataComponent.java
+++ src/main/java/org/generationcp/browser/germplasmlist/GermplasmListDataComponent.java
@@ -16,26 +16,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.generationcp.browser.application.Message;
-import org.generationcp.browser.study.containers.RepresentationDatasetQueryFactory;
-import org.generationcp.browser.study.listeners.StudyButtonClickListener;
 import org.generationcp.commons.vaadin.spring.InternationalizableComponent;
 import org.generationcp.commons.vaadin.spring.SimpleResourceBundleMessageSource;
-import org.generationcp.commons.vaadin.util.MessageNotifier;
-import org.generationcp.middleware.exceptions.QueryException;
 import org.generationcp.middleware.manager.api.GermplasmListManager;
-import org.generationcp.middleware.pojos.Factor;
-import org.generationcp.middleware.pojos.GermplasmList;
 import org.generationcp.middleware.pojos.GermplasmListData;
-import org.generationcp.middleware.pojos.Variate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.vaadin.addons.lazyquerycontainer.LazyQueryContainer;
 
-import com.vaadin.data.Item;
-import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
@@ -46,6 +36,14 @@ public class GermplasmListDataComponent extends VerticalLayout implements Initia
     @SuppressWarnings("unused")
     private static final Logger LOG = LoggerFactory.getLogger(GermplasmListDataComponent.class);
     private static final long serialVersionUID = -6487623269938610915L;
+    
+    private static final String GID = "gid";
+    private static final String ENTRY_ID = "entryId";
+    private static final String ENTRY_CODE = "entryCode";
+    private static final String SEED_SOURCE = "seedSource";
+    private static final String DESIGNATION = "designation";
+    private static final String GROUP_NAME = "groupName";
+    private static final String STATUS = "status";
 
     private Table listDataTable;
     
@@ -64,41 +62,47 @@ public class GermplasmListDataComponent extends VerticalLayout implements Initia
     public void afterPropertiesSet() throws Exception{
         List<GermplasmListData> listData = new ArrayList<GermplasmListData>();
         int listDataCount = this.germplasmListManager.countGermplasmListDataByListId(germplasmListId);
-        listData = this.germplasmListManager.getGermplasmListDataByListId(germplasmListId, 0, listDataCount);
-        
-        // create the Vaadin Table to display the Germplasm List Data
-        listDataTable = new Table("");
-        listDataTable.setColumnCollapsingAllowed(true);
-        listDataTable.setColumnReorderingAllowed(true);
-        listDataTable.setPageLength(15); // number of rows to display in the Table
-        listDataTable.setSizeFull(); // to make scrollbars appear on the Table component
-        
-        listDataTable.addContainerProperty("gid", Integer.class, null);
-        listDataTable.addContainerProperty("entryId", Integer.class, null);
-        listDataTable.addContainerProperty("entryCode", String.class, null);
-        listDataTable.addContainerProperty("seedSource", String.class, null);
-        listDataTable.addContainerProperty("designation", String.class, null);
-        listDataTable.addContainerProperty("groupName", String.class, null);
-        listDataTable.addContainerProperty("status", String.class, null);
-        
-        listDataTable.setColumnHeader("gid", "GID");
-        listDataTable.setColumnHeader("entryId", "Entry ID");
-        listDataTable.setColumnHeader("entryCode", "Entry Code");
-        listDataTable.setColumnHeader("seedSource", "Seed Source");
-        listDataTable.setColumnHeader("designation", "Designation");
-        listDataTable.setColumnHeader("groupName", "Group Name");
-        listDataTable.setColumnHeader("status", "Status");
-        
-        for (GermplasmListData data : listData) {
-            listDataTable.addItem(new Object[] {
-                    data.getGid(), data.getEntryId(), data.getEntryCode(), data.getSeedSource(),
-                    data.getDesignation(), data.getGroupName(), data.getStatusString()
-            }, data.getId());
+        if (listDataCount == 0) {
+            addComponent(new Label(messageSource.getMessage(Message.no_listdata_retrieved_label))); // "No Germplasm List Data retrieved."
+        } else {
+            
+            listData = this.germplasmListManager.getGermplasmListDataByListId(germplasmListId, 0, listDataCount);
+            
+            // create the Vaadin Table to display the Germplasm List Data
+            listDataTable = new Table("");
+            listDataTable.setColumnCollapsingAllowed(true);
+            listDataTable.setColumnReorderingAllowed(true);
+            listDataTable.setPageLength(15); // number of rows to display in the Table
+            listDataTable.setSizeFull(); // to make scrollbars appear on the Table component
+            
+            listDataTable.addContainerProperty(GID, Integer.class, null);
+            listDataTable.addContainerProperty(ENTRY_ID, Integer.class, null);
+            listDataTable.addContainerProperty(ENTRY_CODE, String.class, null);
+            listDataTable.addContainerProperty(SEED_SOURCE, String.class, null);
+            listDataTable.addContainerProperty(DESIGNATION, String.class, null);
+            listDataTable.addContainerProperty(GROUP_NAME, String.class, null);
+            listDataTable.addContainerProperty(STATUS, String.class, null);
+            
+            messageSource.setColumnHeader(listDataTable, GID, Message.listdata_gid_header);
+            messageSource.setColumnHeader(listDataTable, ENTRY_ID, Message.listdata_entryid_header);
+            messageSource.setColumnHeader(listDataTable, ENTRY_CODE, Message.listdata_entrycode_header);
+            messageSource.setColumnHeader(listDataTable, SEED_SOURCE, Message.listdata_seedsource_header);
+            messageSource.setColumnHeader(listDataTable, DESIGNATION, Message.listdata_designation_header);
+            messageSource.setColumnHeader(listDataTable, GROUP_NAME, Message.listdata_groupname_header);
+            messageSource.setColumnHeader(listDataTable, STATUS, Message.listdata_status_header);
+            
+            for (GermplasmListData data : listData) {
+                listDataTable.addItem(new Object[] {
+                        data.getGid(), data.getEntryId(), data.getEntryCode(), data.getSeedSource(),
+                        data.getDesignation(), data.getGroupName(), data.getStatusString()
+                }, data.getId());
+            }
+            
+            listDataTable.sort(new Object[]{"entryId"}, new boolean[]{true});
+    
+            setSpacing(true);
+            addComponent(listDataTable);
         }
-        
-        listDataTable.sort(new Object[]{"entryId"}, new boolean[]{true});
-
-        addComponent(listDataTable);
     }
     
     @Override
@@ -109,12 +113,6 @@ public class GermplasmListDataComponent extends VerticalLayout implements Initia
 
     @Override
     public void updateLabels() {
-        /*messageSource.setCaption(lblName, Message.name_label);
-        messageSource.setCaption(lblTitle, Message.title_label);
-        messageSource.setCaption(lblObjective, Message.objective_label);
-        messageSource.setCaption(lblType, Message.type_label);
-        messageSource.setCaption(lblStartDate, Message.start_date_label);
-        messageSource.setCaption(lblEndDate, Message.end_date_label);*/
     }
 
 }
