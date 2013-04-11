afad7e4174abd8c17760046c720760cf946457b9
kevin <kevin@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-04-11 14:18:01 +0000

    corrected spelling for designation column
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/BreedingManager@2607 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- src/main/java/org/generationcp/breeding/manager/listimport/SpecifyGermplasmDetailsComponent.java
+++ src/main/java/org/generationcp/breeding/manager/listimport/SpecifyGermplasmDetailsComponent.java
@@ -111,7 +111,7 @@ public class SpecifyGermplasmDetailsComponent extends AbsoluteLayout implements 
         germplasmDetailsTable.addContainerProperty(3, String.class, null);
         germplasmDetailsTable.addContainerProperty(4, String.class, null);
         germplasmDetailsTable.addContainerProperty(5, String.class, null);
-        germplasmDetailsTable.setColumnHeaders(new String[]{"Entry ID", "Entry CD", "Desgination", "Cross", "Source"});
+        germplasmDetailsTable.setColumnHeaders(new String[]{"Entry ID", "Entry CD", "Designation", "Cross", "Source"});
         germplasmDetailsTable.setHeight("200px");
         germplasmDetailsTable.setWidth("700px");
         addComponent(germplasmDetailsTable, "top:160px;left:20px");
