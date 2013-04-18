34f2d478a53b2a42017028ed84bc19d5e580267e
alex <alex@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-04-18 13:23:29 +0000

    implemented algorithm for determining the breeding method of a cross based on breeding status of parental lines.
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/BreedingManager@2700 4ebb2e00-0138-4608-bc6e-edf3948d4863

---
+++ src/main/java/org/generationcp/breeding/manager/util/CrossingManagerUtil.java
@@ -0,0 +1,100 @@
+package org.generationcp.breeding.manager.util;
+
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.generationcp.middleware.manager.api.GermplasmDataManager;
+import org.generationcp.middleware.pojos.Germplasm;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class CrossingManagerUtil{
+
+    @Autowired
+    private GermplasmDataManager germplasmDataManager;
+
+
+    public CrossingManagerUtil(GermplasmDataManager germplasmDataManager) {
+	this.germplasmDataManager = germplasmDataManager;
+    }
+
+
+    public Germplasm setCrossingBreedingMethod(Germplasm gc,Integer femaleGid, Integer maleGid) throws MiddlewareQueryException{
+
+	Germplasm gf = germplasmDataManager.getGermplasmByGID(femaleGid); // germplasm female
+	Germplasm gm = germplasmDataManager.getGermplasmByGID(maleGid); // germplasm male
+	Germplasm gff = germplasmDataManager.getGermplasmByGID(gf.getGpid2()); // maternal male grand parent (daddy of female parent)
+	Germplasm gfm =  germplasmDataManager.getGermplasmByGID(gf.getGpid1()); // maternal female grand parent (mommy of female parent)
+	Germplasm gmf = germplasmDataManager.getGermplasmByGID(gm.getGpid1()); //  paternal female grand parent (mommy of male parent)
+	Germplasm gmm =  germplasmDataManager.getGermplasmByGID(gm.getGpid2()); // paternal male grand parent (daddy of male parent)
+
+	if(gf.getGnpgs()<0)
+	{
+	    if(gm.getGnpgs()<0)
+	    {
+		gc.setMethodId(101);
+	    }
+	    else
+	    {
+		if(gm.getGnpgs()==1)
+		{
+		    gc.setMethodId(101);
+		}
+		else if(gm.getGnpgs()==2)
+		{
+		    if(gmf.getGid()==gf.getGid() || gmm.getGid()==gf.getGid())
+		    {
+			gc.setMethodId(107);
+		    }
+		    else
+		    {
+			gc.setMethodId(102);
+		    }
+		}
+		else
+		{
+		    gc.setMethodId(106);
+		}
+	    }
+	}
+	else
+	{
+	    if(gm.getGnpgs()<0)
+	    {
+		if(gf.getGnpgs()==1)
+		{
+		    gc.setMethodId(101);
+		}
+		else if(gf.getGnpgs()==2)
+		{
+		    if(gff.getGid()==gm.getGid() || gfm.getGid()==gm.getGid())
+		    {
+			gc.setMethodId(107);
+		    }
+		    else
+		    {
+			gc.setMethodId(102);
+		    }
+		}
+		else
+		{
+		    gc.setMethodId(106);
+		}
+	    }
+	    else
+	    {
+		if(gf.getMethodId()==101 && gm.getMethodId()==101)
+		{
+		    gc.setMethodId(103);
+		}
+		else
+		{
+		    gc.setMethodId(106);
+		}
+	    }
+	}
+
+	return gc;
+
+    }
+
+
+}
---
+++ src/test/java/org/generationcp/breeding/manager/util/test/TestCrossingManagerUtil.java
@@ -0,0 +1,48 @@
+package org.generationcp.breeding.manager.util.test;
+
+import org.generationcp.breeding.manager.util.CrossingManagerUtil;
+import org.generationcp.middleware.manager.DatabaseConnectionParameters;
+import org.generationcp.middleware.manager.ManagerFactory;
+import org.generationcp.middleware.manager.api.GermplasmDataManager;
+import org.generationcp.middleware.pojos.Germplasm;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TestCrossingManagerUtil{
+
+
+    private static ManagerFactory factory;
+    private static GermplasmDataManager germplasmManager;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+	DatabaseConnectionParameters local = new DatabaseConnectionParameters("IBPDatasource.properties", "local");
+	DatabaseConnectionParameters central = new DatabaseConnectionParameters("IBPDatasource.properties", "central");
+	factory = new ManagerFactory(local, central);
+	germplasmManager = factory.getGermplasmDataManager();
+
+    }
+
+    @Test
+    public void testSetCrossingBreedingMethod() throws Exception {
+	Integer gid = Integer.valueOf(10);
+	Integer femaleGID=Integer.valueOf(50533);
+	Integer maleGID=Integer.valueOf(456);
+	
+	Germplasm GC = germplasmManager.getGermplasmByGID(gid);
+	System.out.println("Germplasm Cross(" + gid + "): " + GC);
+	
+	CrossingManagerUtil cm= new CrossingManagerUtil(germplasmManager);
+	Germplasm g= cm.setCrossingBreedingMethod(GC, femaleGID, maleGID);
+	
+	System.out.println("Germplasm object for the cross with assigned value for the methn field(" + gid + "): " + GC);
+
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+	factory.close();
+    }
+}
