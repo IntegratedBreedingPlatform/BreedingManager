de332118766d66dd993f94b141d388f658a18448
kevin <kevin@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-06-06 08:42:41 +0000

    starting implementation for head to head comparison cross study query tool
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/GermplasmStudyBrowser@3826 4ebb2e00-0138-4608-bc6e-edf3948d4863

---
+++ src/main/java/org/generationcp/browser/cross/study/h2h/HeadToHeadComparisonMain.java
@@ -0,0 +1,68 @@
+package org.generationcp.browser.cross.study.h2h;
+
+import org.generationcp.commons.vaadin.spring.InternationalizableComponent;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import com.vaadin.ui.Accordion;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.PopupView;
+import com.vaadin.ui.VerticalLayout;
+
+@Configurable
+public class HeadToHeadComparisonMain extends VerticalLayout implements InitializingBean, InternationalizableComponent {
+
+	private static final long serialVersionUID = -3488805933508882321L;
+	private static final String VERSION = "2.2.1.0";
+	
+	private Accordion accordion;
+	
+	private HorizontalLayout titleLayout;
+	
+	private Label mainTitle;
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		setMargin(false);
+		setSpacing(true);
+		
+		titleLayout = new HorizontalLayout();
+        titleLayout.setSpacing(true);
+        setTitleContent("");
+        addComponent(titleLayout);
+        
+        accordion = new Accordion();
+        accordion.setWidth("1000px");
+        
+	}
+
+	@Override
+	public void updateLabels() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void setTitleContent(String guideMessage){
+        titleLayout.removeAllComponents();
+        
+        String title =  "<h1>Cross Study:</h1> <h1>Head-to-Head Comparison Query</h1> <h2>" + VERSION + "</h2>";
+        mainTitle = new Label();
+        mainTitle.setStyleName("gcp-window-title");
+        mainTitle.setContentMode(Label.CONTENT_XHTML);
+        mainTitle.setValue(title);
+        titleLayout.addComponent(mainTitle);
+        
+        /**
+        Label descLbl = new Label(guideMessage);
+        descLbl.setWidth("300px");
+        
+        PopupView popup = new PopupView("?",descLbl);
+        popup.setStyleName("gcp-popup-view");
+        titleLayout.addComponent(popup);
+        
+        titleLayout.setComponentAlignment(popup, Alignment.MIDDLE_LEFT);
+        **/
+    }
+}
