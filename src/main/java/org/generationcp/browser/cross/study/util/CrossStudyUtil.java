d9cf044cf4b89256916d57dc46860c7275eee127
Darla Grace Ani <darla@efficio.us.com>
2013-10-02 09:32:14 +0800

    GCP 5361 - created EnvironmentWeight enum and CrossStudyUtil class

---
+++ src/main/java/org/generationcp/browser/cross/study/util/CrossStudyUtil.java
@@ -0,0 +1,31 @@
+package org.generationcp.browser.cross.study.util;
+
+import org.generationcp.browser.cross.study.constants.EnvironmentWeight;
+
+import com.vaadin.ui.ComboBox;
+
+public class CrossStudyUtil {
+	
+	/**
+	 * Creates a combobox with values of <class>EnvironmentWeight</class> enum
+	 * @return
+	 */
+    public static ComboBox getWeightComboBox(){
+    	ComboBox combo = new ComboBox();
+    	combo.setNullSelectionAllowed(false);
+    	combo.setTextInputAllowed(false);
+    	combo.setImmediate(true);
+    	
+    	for (EnvironmentWeight weight : EnvironmentWeight.values()){
+    		combo.addItem(weight);
+    		combo.setItemCaption(weight, weight.getLabel());
+    		
+    	}
+			
+		combo.setValue(EnvironmentWeight.IMPORTANT);
+		
+		combo.setEnabled(false);
+		return combo;
+    }
+
+}
