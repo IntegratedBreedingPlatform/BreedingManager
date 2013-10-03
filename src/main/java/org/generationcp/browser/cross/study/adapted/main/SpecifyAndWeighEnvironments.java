7e34ac0461deee11f02b474b0a02fdf7cd6b95f6
Crisjaira Villarino <crisjaira@efficio.us.com>
2013-10-03 10:35:57 +0800

    GCP-5515 UI

--- src/main/java/org/generationcp/browser/cross/study/adapted/main/SpecifyAndWeighEnvironments.java
+++ src/main/java/org/generationcp/browser/cross/study/adapted/main/SpecifyAndWeighEnvironments.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 
 import com.vaadin.ui.AbsoluteLayout;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
 
 @Configurable
 public class SpecifyAndWeighEnvironments extends AbsoluteLayout implements InitializingBean, InternationalizableComponent {
@@ -18,7 +20,17 @@ public class SpecifyAndWeighEnvironments extends AbsoluteLayout implements Initi
 	private ResultsComponent resultsScreen;
 	
 	private Label headerLabel;
+	private Label headerValLabel;
+	private Label chooseEnvLabel;
+	private Label noOfEnvLabel;
+	private Label noOfEnvValLabel;
 	
+	private Button filterByLocationBtn;
+	private Button filterByStudyBtn;
+	private Button addEnvConditionsBtn;
+	private Button nextBtn;
+	
+	private Table entriesTable;
 	
 	@Autowired
     private SimpleResourceBundleMessageSource messageSource;
@@ -39,7 +51,55 @@ public class SpecifyAndWeighEnvironments extends AbsoluteLayout implements Initi
 	@Override
 	public void afterPropertiesSet() throws Exception {
 	   setHeight("550px");
-       setWidth("1000px");	
+       setWidth("1000px");
+       
+       headerLabel = new Label(messageSource.getMessage(Message.ENVIRONMENT_FILTER));
+       headerLabel.setImmediate(true);
+       addComponent(headerLabel, "top:20px;left:20px");
+       
+       headerValLabel = new Label(messageSource.getMessage(Message.ENVIRONMENT_FILTER_VAL));
+       headerValLabel.setStyleName("gcp-bold-italic");
+       headerValLabel.setContentMode(Label.CONTENT_XHTML);
+       headerValLabel.setImmediate(true);
+       addComponent(headerValLabel, "top:20px;left:150px");
+       
+       
+       filterByLocationBtn = new Button(messageSource.getMessage(Message.FILTER_BY_LOCATION));
+       filterByLocationBtn.setWidth("200px");
+       addComponent(filterByLocationBtn, "top:50px;left:20px");
+       
+       filterByStudyBtn = new Button(messageSource.getMessage(Message.FILTER_BY_STUDY));
+       filterByStudyBtn.setWidth("200px");
+       addComponent(filterByStudyBtn, "top:50px;left:240px");
+       
+       addEnvConditionsBtn = new Button(messageSource.getMessage(Message.ADD_ENV_CONDITION));
+       addEnvConditionsBtn.setWidth("400px");
+       addComponent(addEnvConditionsBtn, "top:50px;left:580px");
+       
+       
+       chooseEnvLabel = new Label(messageSource.getMessage(Message.CHOOSE_ENVIRONMENTS));
+       chooseEnvLabel.setImmediate(true);
+       addComponent(chooseEnvLabel, "top:90px;left:20px");
+       
+       entriesTable = new Table();
+       entriesTable.setWidth("960px");
+       entriesTable.setHeight("370px");
+       entriesTable.setImmediate(true);
+       entriesTable.setPageLength(-1);
+       addComponent(entriesTable, "top:110px;left:20px");
+       
+       noOfEnvLabel = new Label(messageSource.getMessage(Message.NO_OF_SELECTED_ENVIRONMENT));
+       noOfEnvLabel.setImmediate(true);
+       addComponent(noOfEnvLabel, "top:500px;left:20px");
+       
+       noOfEnvValLabel = new Label("0");
+       noOfEnvValLabel.setImmediate(true);
+       addComponent(noOfEnvValLabel, "top:500px;left:230px");
+       
+       nextBtn = new Button("Next");
+       nextBtn.setWidth("100px");
+       nextBtn.setEnabled(false);
+       addComponent(nextBtn, "top:490px;left:880px");
 	}
 
 }
