b3e214e1603ee9dbbb4eb060b60726a1032fa430
dennis <dennis@131db.com>
2013-10-16 11:14:50 +0800

    GCP-5832

---
+++ src/main/java/org/generationcp/browser/cross/study/adapted/main/WelcomeScreen.java
@@ -0,0 +1,68 @@
+package org.generationcp.browser.cross.study.adapted.main;
+
+import org.generationcp.browser.application.Message;
+import org.generationcp.browser.cross.study.adapted.main.listeners.AdaptedGermplasmButtonClickListener;
+import org.generationcp.commons.vaadin.spring.InternationalizableComponent;
+import org.generationcp.commons.vaadin.spring.SimpleResourceBundleMessageSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import com.vaadin.ui.AbsoluteLayout;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+
+@Configurable
+public class WelcomeScreen extends AbsoluteLayout implements InitializingBean, InternationalizableComponent {
+	
+    private static final long serialVersionUID = -3667517088395779496L;
+    
+    private final static Logger LOG = LoggerFactory.getLogger(org.generationcp.browser.cross.study.adapted.main.WelcomeScreen.class);
+    
+    public static final String NEXT_BUTTON_ID = "WelcomeScreen Next Button ID";
+    
+    private QueryForAdaptedGermplasmMain mainScreen;
+	private SpecifyAndWeighEnvironments nextScreen;
+	
+	private Label introductionMessage;
+	
+	private Button nextButton;
+
+	@Autowired
+    private SimpleResourceBundleMessageSource messageSource;
+	
+	public WelcomeScreen(QueryForAdaptedGermplasmMain mainScreen, SpecifyAndWeighEnvironments nextScreen) {
+		 this.mainScreen = mainScreen;
+		 this.nextScreen = nextScreen;
+	}
+
+	@Override
+	public void updateLabels() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+	   setHeight("150px");
+       setWidth("1000px");
+       
+       introductionMessage = new Label(messageSource.getMessage(Message.QUERY_FOR_ADAPTED_GERMPLASM_INTRODUCTION_MESSAGE));
+       addComponent(introductionMessage, "top:30px; left:30px;");
+       
+       nextButton = new Button(messageSource.getMessage(Message.NEXT));
+       nextButton.setData(NEXT_BUTTON_ID);
+       nextButton.addListener(new AdaptedGermplasmButtonClickListener(this));
+       nextButton.setWidth("100px");
+       addComponent(nextButton, "top:90px;left:880px");
+	}
+
+    
+    public void nextButtonClickAction(){
+        this.mainScreen.selectFirstTab();
+    }
+    
+}
+
