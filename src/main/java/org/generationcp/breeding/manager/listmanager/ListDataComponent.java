bf007929e0bad356fde6d34c60242182748b72c8
Darla Grace Ani <darla@efficio.us.com>
2013-11-28 22:32:19 +0800

    GCP-6296

--- src/main/java/org/generationcp/breeding/manager/listmanager/ListDataComponent.java
+++ src/main/java/org/generationcp/breeding/manager/listmanager/ListDataComponent.java
@@ -32,6 +32,7 @@ import org.generationcp.breeding.manager.listmanager.util.AddColumnContextMenu;
 import org.generationcp.breeding.manager.listmanager.util.FillWith;
 import org.generationcp.breeding.manager.listmanager.util.GermplasmListExporter;
 import org.generationcp.breeding.manager.listmanager.util.GermplasmListExporterException;
+import org.generationcp.breeding.manager.listmanager.util.ListDataPropertiesRenderer;
 import org.generationcp.breeding.manager.util.GermplasmDetailModel;
 import org.generationcp.commons.exceptions.InternationalizableException;
 import org.generationcp.commons.util.FileDownloadResource;
@@ -64,11 +65,11 @@ import com.vaadin.data.Container;
 import com.vaadin.data.Item;
 import com.vaadin.data.Property;
 import com.vaadin.event.Action;
-import com.vaadin.event.ItemClickEvent;
 import com.vaadin.event.FieldEvents.BlurEvent;
 import com.vaadin.event.FieldEvents.BlurListener;
 import com.vaadin.event.FieldEvents.FocusEvent;
 import com.vaadin.event.FieldEvents.FocusListener;
+import com.vaadin.event.ItemClickEvent;
 import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.AbsoluteLayout;
@@ -506,6 +507,10 @@ public class ListDataComponent extends VerticalLayout implements InitializingBea
         		,ListDataTablePropertyID.DESIGNATION.getName()
         		,ListDataTablePropertyID.GROUP_NAME.getName()
         		,ListDataTablePropertyID.STATUS.getName()});
+        
+        // render additional columns
+    	ListDataPropertiesRenderer newColumnsRenderer = new ListDataPropertiesRenderer(germplasmListId, listDataTable);
+    	newColumnsRenderer.render();
         
         makeTableEditable();
     }
