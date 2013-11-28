69028f2f79b4be729d876601c5eb3dc0622056fc
Mark Agarrado <macky@efficio.us.com>
2013-11-28 19:01:53 +0800

    GCP-6182 Fill With Attribute

---
+++ src/main/java/org/generationcp/breeding/manager/listmanager/FillWithAttributeWindow.java
@@ -0,0 +1,178 @@
+/*******************************************************************************
+ * Copyright (c) 2012, All Rights Reserved.
+ * 
+ * Generation Challenge Programme (GCP)
+ * 
+ * 
+ * This software is licensed for use under the terms of the GNU General Public
+ * License (http://bit.ly/8Ztv8M) and the provisions of Part F of the Generation
+ * Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
+ * 
+ *******************************************************************************/
+package org.generationcp.breeding.manager.listmanager;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.generationcp.breeding.manager.application.Message;
+import org.generationcp.commons.vaadin.spring.InternationalizableComponent;
+import org.generationcp.commons.vaadin.spring.SimpleResourceBundleMessageSource;
+import org.generationcp.middleware.exceptions.MiddlewareQueryException;
+import org.generationcp.middleware.manager.api.GermplasmDataManager;
+import org.generationcp.middleware.pojos.UserDefinedField;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.Window;
+
+
+/**
+ * This class opens a pop-up window for selecting attributes
+ * 
+ * @author Mark Agarrado
+ */
+
+@Configurable
+public class FillWithAttributeWindow extends Window implements InternationalizableComponent, InitializingBean {
+
+    private static final long serialVersionUID = -8850686249688989080L;
+    
+    private SimpleResourceBundleMessageSource messageSource;
+    
+    //private Window mainWindow;
+    private Table targetTable;
+    private String gidPropertyId;
+    private String targetPropertyId;
+    private HorizontalLayout attributeLayout;
+    private ComboBox attributeBox;
+    private Button okButton;
+    private List<UserDefinedField> attributeList;
+    
+    @Autowired
+    private GermplasmDataManager germplasmDataManager;
+    
+    public FillWithAttributeWindow(Table targetTable, String gidPropertyId, String targetPropertyId, SimpleResourceBundleMessageSource messageSource) {
+        this.targetTable = targetTable;
+        this.gidPropertyId = gidPropertyId;
+        this.targetPropertyId = targetPropertyId;
+        this.messageSource = messageSource;
+    }
+    
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        assemble();
+    }
+    
+    protected void assemble() {
+        initializeComponents();
+        initializeValues();
+        initializeLayout();
+        initializeActions();
+    }
+    
+    private void initializeComponents() {
+        attributeBox = new ComboBox();
+        okButton = new Button();
+    }
+    
+    private void initializeValues() {
+        try {
+            List<Integer> gids = getGidsFromTable(targetTable);
+            attributeList = germplasmDataManager.getAttributeTypesByGIDList(gids);
+            
+            for (UserDefinedField attribute : attributeList) {
+                attributeBox.addItem(attribute.getFldno());
+                attributeBox.setItemCaption(attribute.getFldno(), attribute.getFname());
+            }
+        } catch (MiddlewareQueryException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    private void initializeLayout() {
+        attributeBox.setWidth("310px");
+        
+        attributeLayout = new HorizontalLayout();
+        attributeLayout.setMargin(true);
+        attributeLayout.setSpacing(true);
+        
+        attributeLayout.addComponent(attributeBox);
+        attributeLayout.addComponent(okButton);
+        
+        //set window properties
+        setContent(attributeLayout);
+        setWidth("400px");
+        setHeight("30px");
+        center();
+        setResizable(false);
+        setModal(true);
+    }
+    
+    private void initializeActions() {
+        okButton.addListener(new ClickListener() {
+            private static final long serialVersionUID = -7472646361265849940L;
+            @Override
+            public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
+                fillWithAttribute((Integer) attributeBox.getValue());
+                // close pop-up
+                Window attributeWindow = ((Button) event.getSource()).getWindow();
+                attributeWindow.getParent().removeWindow(attributeWindow);
+            }
+        });
+    }
+    
+    private void fillWithAttribute(Integer attributeType){
+        if (attributeType!=null) {
+            try {
+                List<Integer> gids = getGidsFromTable(targetTable);
+                Map<Integer, String> gidAttributeMap = germplasmDataManager.getAttributeValuesByTypeAndGIDList(attributeType, gids);
+                
+                List<Integer> itemIds = getItemIds(targetTable);
+                for(Integer itemId: itemIds){
+                    Integer gid = Integer.valueOf(((Button) targetTable.getItem(itemId).getItemProperty(gidPropertyId).getValue()).getCaption().toString());
+                    targetTable.getItem(itemId).getItemProperty(targetPropertyId).setValue(gidAttributeMap.get(gid));
+                }
+            } catch (MiddlewareQueryException e) {
+                e.printStackTrace();
+            }
+        }
+     }
+    
+    private List<Integer> getGidsFromTable(Table table){
+        List<Integer> gids = new ArrayList<Integer>();
+        List<Integer> listDataItemIds = getItemIds(table);
+        for(Integer itemId: listDataItemIds){
+            gids.add(Integer.valueOf(((Button) table.getItem(itemId).getItemProperty(gidPropertyId).getValue()).getCaption().toString()));
+        }
+        return gids;
+    }
+   
+    @SuppressWarnings("unchecked")
+    private List<Integer> getItemIds(Table table){
+        List<Integer> itemIds = new ArrayList<Integer>();
+        itemIds.addAll((Collection<? extends Integer>) table.getItemIds());
+        return itemIds;
+    }
+    
+    @Override
+    public void attach() {      
+        super.attach();
+        updateLabels();
+    }
+    
+    @Override
+    public void updateLabels() {
+        messageSource.setCaption(this, Message.FILL_WITH_ATTRIBUTE_WINDOW);
+        messageSource.setCaption(okButton, Message.OK);
+    }
+
+}
--- src/main/java/org/generationcp/breeding/manager/listmanager/util/FillWith.java
+++ src/main/java/org/generationcp/breeding/manager/listmanager/util/FillWith.java
@@ -11,6 +11,7 @@ import org.generationcp.breeding.manager.listmanager.BuildNewListComponent;
 import org.generationcp.breeding.manager.listmanager.ListDataComponent;
 import org.generationcp.breeding.manager.listmanager.ListManagerTreeMenu;
 import org.generationcp.breeding.manager.listmanager.constants.ListDataTablePropertyID;
+import org.generationcp.breeding.manager.listmanager.FillWithAttributeWindow;
 import org.generationcp.breeding.manager.util.GermplasmDetailModel;
 import org.generationcp.commons.exceptions.InternationalizableException;
 import org.generationcp.commons.vaadin.spring.InternationalizableComponent;
@@ -60,6 +61,7 @@ public class FillWith implements InternationalizableComponent  {
 	private ContextMenuItem menuFillWithGermplasmDate;
 	private ContextMenuItem menuFillWithPrefName;
 	private ContextMenuItem menuFillWithPrefID;
+	private ContextMenuItem menuFillWithAttribute;
 	private ContextMenuItem menuFillWithLocationName;
 	private ContextMenuItem menuFillWithBreedingMethodInfo;
 	private ContextMenuItem menuFillWithBreedingMethodName;
@@ -123,6 +125,7 @@ public class FillWith implements InternationalizableComponent  {
 	   	 menuFillWithPrefID = fillWithMenu.addItem(messageSource.getMessage(Message.FILL_WITH_PREF_ID));
 	   	 menuFillWithGermplasmDate = fillWithMenu.addItem(messageSource.getMessage(Message.FILL_WITH_GERMPLASM_DATE));
 	   	 menuFillWithPrefName = fillWithMenu.addItem(messageSource.getMessage(Message.FILL_WITH_PREF_NAME));
+	   	 menuFillWithAttribute = fillWithMenu.addItem(messageSource.getMessage(Message.FILL_WITH_ATTRIBUTE));
 	   	 
 	   	 menuFillWithBreedingMethodInfo = fillWithMenu.addItem(messageSource.getMessage(Message.FILL_WITH_BREEDING_METHOD_INFO));
 	   	 menuFillWithBreedingMethodName = menuFillWithBreedingMethodInfo.addItem(messageSource.getMessage(Message.FILL_WITH_BREEDING_METHOD_NAME));
@@ -159,6 +162,8 @@ public class FillWith implements InternationalizableComponent  {
 		   				 fillWithPreferredName();
 		   			 }else if(clickedItem.getName().equals(messageSource.getMessage(Message.FILL_WITH_PREF_ID))){
 		   				 fillWithPreferredID();
+		   			 }else if(clickedItem.getName().equals(messageSource.getMessage(Message.FILL_WITH_ATTRIBUTE))){
+		   			     fillWithAttribute(targetTable, (String) fillWithMenu.getData());
 	   				 }else if(clickedItem.getName().equals(messageSource.getMessage(Message.FILL_WITH_BREEDING_METHOD_NAME))){
 		   				 fillWithMethodName(targetTable, (String) fillWithMenu.getData());
 		   			 } else if(clickedItem.getName().equals(messageSource.getMessage(Message.FILL_WITH_BREEDING_METHOD_ABBREVIATION))){
@@ -238,6 +243,12 @@ public class FillWith implements InternationalizableComponent  {
        for(Integer itemId: itemIds){
            table.getItem(itemId).getItemProperty(propertyId).setValue("");
        }
+    }
+    
+    public void fillWithAttribute(Table table, String propertyId) {
+        Window mainWindow = table.getWindow();
+        Window attributeWindow = new FillWithAttributeWindow(table, GIDPropertyId, propertyId, messageSource);
+        mainWindow.addWindow(attributeWindow);
     }
     
 	public void fillWithGermplasmDate(Table table, String propertyId){
