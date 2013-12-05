4aa7de12f02e2c0d2d0c2688e22f2930c6316e5d
Darla Grace Ani <darla@efficio.us.com>
2013-12-05 14:33:28 +0800

    GCP-6166 remove validation that prefix should not contain whitespace. There are cases where germplasm name prefixes have whitespace.

--- src/main/java/org/generationcp/breeding/manager/crossingmanager/AdditionalDetailsCrossNameComponent.java
+++ src/main/java/org/generationcp/breeding/manager/crossingmanager/AdditionalDetailsCrossNameComponent.java
@@ -249,11 +249,11 @@ public class AdditionalDetailsCrossNameComponent extends AbsoluteLayout
             		messageSource.getMessage(Message.ERROR_ENTER_PREFIX_FIRST), Notification.POSITION_CENTERED);
             return false;
         
-        } else if (prefix.contains(" ")){
-            MessageNotifier.showError(window, messageSource.getMessage(Message.ERROR_WITH_CROSS_CODE), 
-            		messageSource.getMessage(Message.ERROR_PREFIX_HAS_WHITESPACE), Notification.POSITION_CENTERED);
-            return false;
-        
+//        } else if (prefix.contains(" ")){
+//            MessageNotifier.showError(window, messageSource.getMessage(Message.ERROR_WITH_CROSS_CODE), 
+//            		messageSource.getMessage(Message.ERROR_PREFIX_HAS_WHITESPACE), Notification.POSITION_CENTERED);
+//            return false;
+//        
         } else if (prefix.substring(prefix.length()-1).matches("\\d")){
             MessageNotifier.showError(window, messageSource.getMessage(Message.ERROR_WITH_CROSS_CODE),  
             		messageSource.getMessage(Message.ERROR_PREFIX_ENDS_IN_NUMBER), Notification.POSITION_CENTERED);
