91ba46aaab5a38d765a5727b11d79421a550f5eb
Crisjaira Villarino <crisjaira@efficio.us.com>
2014-01-30 15:18:46 +0800

    GCP-7054 Mark Changes when Uses Drag and Drop to Drop Handler

--- src/main/java/org/generationcp/breeding/manager/listmanager/DropHandlerComponent.java
+++ src/main/java/org/generationcp/breeding/manager/listmanager/DropHandlerComponent.java
@@ -102,6 +102,7 @@ public class DropHandlerComponent extends Panel {
 				
 				source.getBuildListComponent().setFromDropHandler(true);
 				source.getBuildListComponent().handleDrop(sourceTable, transferable, dropData);
+				source.getBuildListComponent().setHasChanges(true);
 			}
 
 			@Override
