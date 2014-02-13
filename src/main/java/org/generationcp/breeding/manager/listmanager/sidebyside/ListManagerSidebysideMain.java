18ebe79bda6109ea50edc3cf049fa0a067d83523
dennis <dennis@131db.com>
2014-02-13 13:30:59 +0800

    side by side panels test / basic framework

--- src/main/java/org/generationcp/breeding/manager/listmanager/sidebyside/ListManagerSidebysideMain.java
+++ src/main/java/org/generationcp/breeding/manager/listmanager/sidebyside/ListManagerSidebysideMain.java
@@ -3,27 +3,67 @@ package org.generationcp.breeding.manager.listmanager.sidebyside;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Configurable;
 
+import com.vaadin.terminal.Sizeable;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.VerticalSplitPanel;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 
 @Configurable
 public class ListManagerSidebysideMain extends VerticalLayout implements InitializingBean{
 
     private static final long serialVersionUID = 5976245899964745758L;
+	private VerticalSplitPanel vsp;
+	private HorizontalSplitPanel hsp;
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        HorizontalSplitPanel horiSplitPanel = new HorizontalSplitPanel();
-        horiSplitPanel.setSplitPosition(50); // percent
-        
-        // left component:
-        horiSplitPanel.addComponent(new Label("Left side"));
 
-        // right component:
-        horiSplitPanel.addComponent(new Label("Right side"));
+		Label a = new Label("A");
+		Label b = new Label("B");
+		Label c = new Label("C");
+
+		Button A = new Button();
+		A.addListener(new ClickListener(){
+			public void buttonClick(ClickEvent event) {
+				expandTop();
+			}
+		});
+		Button B = new Button();
+		B.addListener(new ClickListener(){
+			public void buttonClick(ClickEvent event) {
+				expandBottom();
+			}
+		});
+		
+		System.out.println(this.getHeight());
+		
+		hsp = new HorizontalSplitPanel();
+		hsp.setHeight("700px");
+		hsp.setSplitPosition(28);
+		
+		vsp = new VerticalSplitPanel();
+		vsp.setHeight("700px");
+		expandTop();
+
+		vsp.setFirstComponent(A);
+		vsp.setSecondComponent(B);
+		
+		hsp.setFirstComponent(vsp);
+		hsp.setSecondComponent(c);
         
-        this.addComponent(horiSplitPanel);
+        this.addComponent(hsp);
+    }
+    
+    private void expandTop(){
+    	vsp.setSplitPosition(vsp.getHeight()-33, Sizeable.UNITS_PIXELS);
     }
 
+    private void expandBottom(){
+    	vsp.setSplitPosition(33, Sizeable.UNITS_PIXELS);
+    }
 }
