c34efde8e2e8bb31591d3dc04df825e38086e68f
Mark Agarrado <macky@efficio.us.com>
2014-03-18 14:48:06 +0800

    Skeleton code for list manager details layout / tabsheet

---
+++ src/main/java/org/generationcp/breeding/manager/listmanager/sidebyside/ListManagerDetailsLayout.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ * Copyright (c) 2012, All Rights Reserved.
+ * 
+ * Generation Challenge Programme (GCP)
+ * 
+ * 
+ * This software is licensed for use under the terms of the GNU General Public
+ * License (http://bit.ly/8Ztv8M) and the provisions of Part F of the Generation
+ * Challenge Programme Amended Consortium Agreement (http://bit.ly/KQX1nL)
+ * 
+ *******************************************************************************/
+package org.generationcp.breeding.manager.listmanager.sidebyside;
+
+import org.generationcp.breeding.manager.application.BreedingManagerLayout;
+import org.generationcp.commons.vaadin.spring.InternationalizableComponent;
+import org.springframework.beans.factory.InitializingBean;
+
+import com.vaadin.ui.VerticalLayout;
+
+
+/**
+ * @author Mark Agarrado
+ *
+ */
+public class ListManagerDetailsLayout extends VerticalLayout implements InternationalizableComponent, InitializingBean,
+        BreedingManagerLayout{
+
+    /* (non-Javadoc)
+     * @see org.generationcp.breeding.manager.application.BreedingManagerLayout#instantiateComponents()
+     */
+    @Override
+    public void instantiateComponents() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.generationcp.breeding.manager.application.BreedingManagerLayout#initializeValues()
+     */
+    @Override
+    public void initializeValues() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.generationcp.breeding.manager.application.BreedingManagerLayout#addListeners()
+     */
+    @Override
+    public void addListeners() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.generationcp.breeding.manager.application.BreedingManagerLayout#layoutComponents()
+     */
+    @Override
+    public void layoutComponents() {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.generationcp.commons.vaadin.spring.InternationalizableComponent#updateLabels()
+     */
+    @Override
+    public void updateLabels() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
