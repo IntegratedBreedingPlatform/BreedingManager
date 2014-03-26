0d0307f9536ab5ffd42fa93139fef6f1154529f6
Mark Agarrado <macky@efficio.us.com>
2014-03-26 09:39:39 +0800

    GCP-7812 Fixed missing list entries section for IE 10

--- src/main/java/org/generationcp/breeding/manager/listmanager/sidebyside/ListManagerDetailsLayout.java
+++ src/main/java/org/generationcp/breeding/manager/listmanager/sidebyside/ListManagerDetailsLayout.java
@@ -144,9 +144,16 @@ public class ListManagerDetailsLayout extends VerticalLayout implements Internat
         headingBar.setVisible(false);
         detailsTabSheet.setVisible(false);
         this.addComponent(noListLabel);
+        this.setComponentAlignment(noListLabel, Alignment.TOP_LEFT);
+        this.setExpandRatio(noListLabel, 1.0f);
         this.addComponent(headingBar);
+        this.setComponentAlignment(headingBar, Alignment.TOP_LEFT);
+        this.setExpandRatio(headingBar, 1.0f);
         this.addComponent(detailsTabSheet);
+        this.setComponentAlignment(detailsTabSheet, Alignment.TOP_LEFT);
+        this.setExpandRatio(detailsTabSheet, 100.0f);
         
+        this.setHeight("800px");
         this.setWidth("98%");
         this.setStyleName(Runo.TABSHEET_SMALL);
         this.setMargin(false);
