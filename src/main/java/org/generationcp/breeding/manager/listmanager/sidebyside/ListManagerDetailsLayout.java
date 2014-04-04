175c8301f6db52c1575034435ba5f06b1a6631e2
Crisjaira Villarino <crisjaira@efficio.us.com>
2014-04-04 13:06:00 +0800

    List Manager Ui Fixes in IE

--- src/main/java/org/generationcp/breeding/manager/listmanager/sidebyside/ListManagerDetailsLayout.java
+++ src/main/java/org/generationcp/breeding/manager/listmanager/sidebyside/ListManagerDetailsLayout.java
@@ -111,7 +111,6 @@ public class ListManagerDetailsLayout extends VerticalLayout implements Internat
     @Override
     public void instantiateComponents() {
     	detailsTabSheet = new TabSheet();
-    	detailsTabSheet.setHeight("550px");
     	
         noListLabel = new Label();
         noListLabel.setImmediate(true);
@@ -158,6 +157,7 @@ public class ListManagerDetailsLayout extends VerticalLayout implements Internat
     	
     	innerLayout = new VerticalLayout();
     	innerLayout.setSpacing(true);
+    	innerLayout.setHeight("100%");
     	
     	//Components
         headingBar.setWidth("100%");
@@ -174,8 +174,8 @@ public class ListManagerDetailsLayout extends VerticalLayout implements Internat
         
         addComponent(innerLayout);
         addComponent(detailsTabSheet);
-        setExpandRatio(innerLayout, 1);
-        setExpandRatio(detailsTabSheet, 99);
+        setExpandRatio(innerLayout, 1.0f);
+        setExpandRatio(detailsTabSheet, 9.0f);
         displayDefault();
     }
     
