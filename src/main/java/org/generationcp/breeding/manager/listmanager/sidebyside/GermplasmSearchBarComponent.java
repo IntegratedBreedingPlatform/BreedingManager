8cbadef572cf02c8ac014cc654c3919e3c9bf79b
dennis <dennis@131db.com>
2014-07-22 07:35:53 +0800

    Added auto highlight on search fields (search for gemrplasm, and view germplasm tab)  Ticket: GCP-8987

--- src/main/java/org/generationcp/breeding/manager/listmanager/sidebyside/GermplasmSearchBarComponent.java
+++ src/main/java/org/generationcp/breeding/manager/listmanager/sidebyside/GermplasmSearchBarComponent.java
@@ -176,6 +176,7 @@ public class GermplasmSearchBarComponent extends CssLayout implements Internatio
 
 	public void focusOnSearchField(){
 		searchField.focus();
+		searchField.selectAll();
 	}
 	
 	@Override
