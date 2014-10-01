60bc1ff68a61eca5e37c360325fc33ea9dc96f09
Daniel Jao <daniel@efficio.us.com>
2014-10-01 14:31:03 +0800

    Added new POJO to abstract the same functionalities of the tree and tree table so we can just use a single super class in our code
    GCP-9409
    
    Reviewer: Daniel Villafuerte

---
+++ src/main/java/org/generationcp/breeding/manager/customcomponent/GermplasmListTree.java
@@ -0,0 +1,36 @@
+package org.generationcp.breeding.manager.customcomponent;
+
+import com.vaadin.ui.Component;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.Tree;
+
+public class GermplasmListTree extends Tree implements GermplasmListSource {
+    public GermplasmListTree(){
+        super();
+    }
+
+    @Override
+    public void setDragMode(TreeDragMode treeDragMode, Table.TableDragMode treeTableDragMode) {
+       super.setDragMode(treeDragMode);
+    }
+
+    @Override
+    public Object addItem(Object[] cells, Object itemId){
+        return super.addItem(itemId);
+    }
+
+    @Override
+    public void setColumnExpandRatio(Object propertyId, float expandRatio) {
+        //not needed to implement any code, just added the function so we would have override for the treetable specific function
+    }
+
+    @Override
+    public Component getUIComponent() {
+        return this;
+    }
+
+    @Override
+    public void setColumnWidth(Object propertyId, int width) {
+        //not needed to implement any code, just added the function so we would have override for the treetable specific function
+    }
+}
