549afda27b32beb54a6ddf4f7437faebf15bf171
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2014-10-06 17:35:33 +0800

    Created a test class for BreedingManagerYearField, testing its minimum and maximum value.
    GCP-10135
    
    Reviewer: Chezka Arevalo

---
+++ src/test/java/org/generationcp/breeding/manager/customfields/test/BreedingManagerYearFieldTest.java
@@ -0,0 +1,43 @@
+package org.generationcp.breeding.manager.customfields.test;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.GregorianCalendar;
+
+import org.generationcp.breeding.manager.customfields.BreedingManagerYearField;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BreedingManagerYearFieldTest {
+
+	private static final Integer RANGE_INTERVAL_FROM_BASE_YEAR = 30;
+
+	@Test
+	public void testHarvestYearFieldHasMinOf30YearsEarlierThanTheCurrentYear() {
+
+		Calendar calendar = new GregorianCalendar();
+		Integer currentYear = calendar.get(Calendar.YEAR);
+		BreedingManagerYearField yearField = new BreedingManagerYearField(currentYear);
+
+		Integer expectedMinYear = currentYear
+				- BreedingManagerYearFieldTest.RANGE_INTERVAL_FROM_BASE_YEAR;
+		Collection<?> yearOptions = yearField.getItemIds();
+		Assert.assertTrue("Expecting that the year field given the base year is the current year, "
+				+ currentYear + ", has " + expectedMinYear + " for " + "minimum year.",
+				yearOptions.contains(expectedMinYear));
+	}
+
+	@Test
+	public void testHarvestYearFieldHasMaxOf30YearsLaterThanTheCurrentYear() {
+		Calendar calendar = new GregorianCalendar();
+		Integer currentYear = calendar.get(Calendar.YEAR);
+		BreedingManagerYearField yearField = new BreedingManagerYearField(currentYear);
+
+		Integer expectedMaxYear = currentYear
+				+ BreedingManagerYearFieldTest.RANGE_INTERVAL_FROM_BASE_YEAR;
+		Collection<?> yearOptions = yearField.getItemIds();
+		Assert.assertTrue("Expecting that the year field given the base year is the current year, "
+				+ currentYear + ", has " + expectedMaxYear + " for " + "maximum year.",
+				yearOptions.contains(expectedMaxYear));
+	}
+}
