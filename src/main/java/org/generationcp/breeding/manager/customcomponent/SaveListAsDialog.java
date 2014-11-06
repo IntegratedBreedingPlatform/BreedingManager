fdf7c801a782df6ecc939fd2dae40b801a958153
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2014-11-06 14:32:15 +0800

    - Extract the date field validation to isListDateValid() and date field retrieval to getCurrentParsedListDate() from doSaveAction() method - Extract the boolean conditions from doSaveAction to isSelectedListAnExistingListButNotItself() and isSelectedListLocked()
    GCP-10337
    
    Reviewer: Mae Isabelle Turiana

--- src/main/java/org/generationcp/breeding/manager/customcomponent/SaveListAsDialog.java
+++ src/main/java/org/generationcp/breeding/manager/customcomponent/SaveListAsDialog.java
@@ -10,6 +10,7 @@ import org.generationcp.breeding.manager.application.Message;
 import org.generationcp.breeding.manager.crossingmanager.ParentTabComponent;
 import org.generationcp.breeding.manager.crossingmanager.listeners.SelectTreeItemOnSaveListener;
 import org.generationcp.breeding.manager.customfields.BreedingManagerListDetailsComponent;
+import org.generationcp.breeding.manager.customfields.ListDateField;
 import org.generationcp.breeding.manager.customfields.LocalListFoldersTreeComponent;
 import org.generationcp.breeding.manager.inventory.ReserveInventoryAction;
 import org.generationcp.breeding.manager.inventory.ReserveInventorySource;
@@ -45,6 +46,7 @@ import com.vaadin.ui.themes.Reindeer;
 @Configurable
 public class SaveListAsDialog extends BaseSubWindow implements InitializingBean, InternationalizableComponent, BreedingManagerLayout{
 
+	private static final String FOLDER_TYPE = "FOLDER";
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOG = LoggerFactory.getLogger(SaveListAsDialog.class);
 	
@@ -161,92 +163,10 @@ public class SaveListAsDialog extends BaseSubWindow implements InitializingBean,
 		cancelButton.addListener(new CloseWindowAction());
 		saveButton.addListener(new ClickListener(){
 			private static final long serialVersionUID = 993268331611479850L;
-
 			@Override
 			public void buttonClick(final ClickEvent event) {
-				
-				//Call method so that the variables will be updated, values will be used for the logic below
-				getGermplasmListToSave();
-				
-				SimpleDateFormat formatter = new SimpleDateFormat(DateUtil.DATE_AS_NUMBER_FORMAT);
-				
-				try {
-				    listDetailsComponent.getListDateField().validate();
-				} catch (InvalidValueException e) {
-					MessageNotifier.showRequiredFieldError(getWindow().getParent().getWindow(), e.getMessage());
-					return;
-				}
-				
-				Date date;
-				try {
-					date = new SimpleDateFormat("E MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH).parse(listDetailsComponent.getListDateField().getValue().toString());
-				} catch (ParseException e) {
-					date = new Date();
-				}
-				
-				//If target list is locked
-				if(germplasmList!=null && germplasmList.getStatus()>=100) {
-					MessageNotifier.showError(getWindow().getParent().getWindow(), 
-							messageSource.getMessage(Message.ERROR), messageSource.getMessage(Message.UNABLE_TO_EDIT_LOCKED_LIST));
-				
-				//If target list to be overwritten is not itself and is an existing list
-				} else if( (germplasmList.getType()!=null && !germplasmList.getType().equals("FOLDER") && (germplasmList.getId()!=null && originalGermplasmList==null)) 
-						|| (germplasmList.getId()!=null && originalGermplasmList!=null &&  germplasmList.getId()!=originalGermplasmList.getId())) {
-					
-					final GermplasmList gl = getGermplasmListToSave();
-					gl.setName(listDetailsComponent.getListNameField().getValue().toString());
-					gl.setDescription(listDetailsComponent.getListDescriptionField().getValue().toString());
-					gl.setType(listDetailsComponent.getListTypeField().getValue().toString());
-					gl.setDate(Long.parseLong(formatter.format(date)));
-					gl.setNotes(listDetailsComponent.getListNotesField().getValue().toString());
-					
-		            ConfirmDialog.show(getWindow().getParent().getWindow(), messageSource.getMessage(Message.DO_YOU_WANT_TO_OVERWRITE_THIS_LIST)+"?", 
-			                messageSource.getMessage(Message.LIST_DATA_WILL_BE_DELETED_AND_WILL_BE_REPLACED_WITH_THE_DATA_FROM_THE_LIST_THAT_YOU_JUST_CREATED), 
-			                messageSource.getMessage(Message.OK), messageSource.getMessage(Message.CANCEL), 
-			                new ConfirmDialog.Listener() {
-								private static final long serialVersionUID = 1L;
-								public void onClose(ConfirmDialog dialog) {
-			                        if (dialog.isConfirmed()) {
-			    						source.saveList(gl);
-			    						saveReservationChanges();
-			    						Window window = event.getButton().getWindow();
-			    				        window.getParent().removeWindow(window);
-			                        }
-			                    }
-			                }
-			            );
-		            
-		        //If target list to be overwritten is itself
-				} else {
-					if(validateAllFields()){
-						
-						GermplasmList gl = getGermplasmListToSave();
-						gl.setName(listDetailsComponent.getListNameField().getValue().toString());
-						gl.setDescription(listDetailsComponent.getListDescriptionField().getValue().toString());
-						gl.setType(listDetailsComponent.getListTypeField().getValue().toString());
-						gl.setDate(Long.parseLong(formatter.format(date)));
-						gl.setNotes(listDetailsComponent.getListNotesField().getValue().toString());
-						
-						source.saveList(gl);
-						saveReservationChanges();
-						
-						Window window = event.getButton().getWindow();
-				        window.getParent().removeWindow(window);
-					}
-				}
-				
-				if(source instanceof ReserveInventorySource){
-					ReserveInventoryAction reserveInventoryAction = new ReserveInventoryAction((ReserveInventorySource) source);
-					if(source instanceof ParentTabComponent){
-						boolean success = reserveInventoryAction.saveReserveTransactions(((ParentTabComponent) source).getValidReservationsToSave(), germplasmList.getId());
-						if(success){
-							((ParentTabComponent)source).refreshInventoryColumns(((ParentTabComponent)source).getValidReservationsToSave());
-							((ParentTabComponent)source).resetListInventoryTableValues();
-						}
-					}
-				}
-			}
-			
+				doSaveAction(event);
+			}			
 		});
 	}
 
@@ -293,6 +213,7 @@ public class SaveListAsDialog extends BaseSubWindow implements InitializingBean,
 
 	@Override
 	public void updateLabels() {
+		// do nothing
 	}
 	
 	public GermplasmList getSelectedListOnTree(){
@@ -307,7 +228,6 @@ public class SaveListAsDialog extends BaseSubWindow implements InitializingBean,
 				folder = germplasmListManager.getGermplasmListById(folderId);
 			} catch (MiddlewareQueryException e) {
 				LOG.error("Error with retrieving list with id: " + folderId, e);
-				e.printStackTrace();
 			}
 		}
 		
@@ -323,7 +243,7 @@ public class SaveListAsDialog extends BaseSubWindow implements InitializingBean,
 		GermplasmList selectedList = getSelectedListOnTree();
 		
 		//If selected item on list/folder tree is a list, use that as target germplasm list
-		if(selectedList!=null && !selectedList.getType().equals("FOLDER")){
+		if(selectedList!=null && !FOLDER_TYPE.equalsIgnoreCase(selectedList.getType())){
 			germplasmList = getSelectedListOnTree();
 			
 			//Needed for overwriting
@@ -334,7 +254,6 @@ public class SaveListAsDialog extends BaseSubWindow implements InitializingBean,
 				selectedList = germplasmListManager.getGermplasmListById(selectedList.getParentId());
 			} catch (MiddlewareQueryException e) {
 				LOG.error("Error with getting parent list: " + selectedList.getParentId(), e);
-				e.printStackTrace();
 			}
 			
 		//If not, use old method, get germplasm list the old way
@@ -389,5 +308,137 @@ public class SaveListAsDialog extends BaseSubWindow implements InitializingBean,
 
 	protected void setShowFoldersOnlyInListTree(boolean showFoldersOnlyInListTree) {
 		this.showFoldersOnlyInListTree = showFoldersOnlyInListTree;
+	}
+
+	public GermplasmList getOriginalGermplasmList() {
+		return originalGermplasmList;
+	}
+
+	public void setOriginalGermplasmList(GermplasmList originalGermplasmList) {
+		this.originalGermplasmList = originalGermplasmList;
+	}
+
+	private void doSaveAction(final ClickEvent event) {
+		//Call method so that the variables will be updated, values will be used for the logic below
+		germplasmList = getGermplasmListToSave();
+		
+		if(isListDateValid(listDetailsComponent.getListDateField())){
+			//If target list is locked
+			if(isSelectedListLocked()) {
+				MessageNotifier.showError(getWindow().getParent().getWindow(), 
+						messageSource.getMessage(Message.ERROR), messageSource.getMessage(Message.UNABLE_TO_EDIT_LOCKED_LIST));
+			
+			//If target list to be overwritten is not itself and is an existing list
+			} else if(isSelectedListAnExistingListButNotItself()) {
+				
+				final GermplasmList gl = getGermplasmListToSave();
+				setGermplasmListDetails(gl);
+				
+			    ConfirmDialog.show(getWindow().getParent().getWindow(), messageSource.getMessage(Message.DO_YOU_WANT_TO_OVERWRITE_THIS_LIST)+"?", 
+			            messageSource.getMessage(Message.LIST_DATA_WILL_BE_DELETED_AND_WILL_BE_REPLACED_WITH_THE_DATA_FROM_THE_LIST_THAT_YOU_JUST_CREATED), 
+			            messageSource.getMessage(Message.OK), messageSource.getMessage(Message.CANCEL), 
+			            new ConfirmDialog.Listener() {
+							private static final long serialVersionUID = 1L;
+							public void onClose(ConfirmDialog dialog) {
+			                    if (dialog.isConfirmed()) {
+									source.saveList(gl);
+									saveReservationChanges();
+									Window window = event.getButton().getWindow();
+							        window.getParent().removeWindow(window);
+			                    }
+			                }
+			            }
+			        );
+			    
+			//If target list to be overwritten is itself
+			} else {
+				if(validateAllFields()){
+					
+					GermplasmList gl = getGermplasmListToSave();
+					setGermplasmListDetails(gl);
+					
+					source.saveList(gl);
+					saveReservationChanges();
+					
+					Window window = event.getButton().getWindow();
+			        window.getParent().removeWindow(window);
+				}
+			}
+			
+			updateInventoryColumnsOnListDataAndListInventoryTables();
+		} 
+	}
+
+	private void updateInventoryColumnsOnListDataAndListInventoryTables() {
+		if(source instanceof ReserveInventorySource){
+			ReserveInventoryAction reserveInventoryAction = new ReserveInventoryAction((ReserveInventorySource) source);
+			if(source instanceof ParentTabComponent){
+				boolean success = reserveInventoryAction.saveReserveTransactions(((ParentTabComponent) source).getValidReservationsToSave(), germplasmList.getId());
+				if(success){
+					((ParentTabComponent)source).refreshInventoryColumns(((ParentTabComponent)source).getValidReservationsToSave());
+					((ParentTabComponent)source).resetListInventoryTableValues();
+				}
+			}
+		}
+	}
+
+	private void setGermplasmListDetails(final GermplasmList gl) {
+		gl.setName(listDetailsComponent.getListNameField().getValue().toString());
+		gl.setDescription(listDetailsComponent.getListDescriptionField().getValue().toString());
+		gl.setType(listDetailsComponent.getListTypeField().getValue().toString());
+		gl.setDate(getCurrentParsedListDate(listDetailsComponent.getListDateField().getValue().toString()));
+		gl.setNotes(listDetailsComponent.getListNotesField().getValue().toString());
+	}
+
+	private boolean isSelectedListAnExistingListButNotItself() {
+		return isSelectedListAnExistingList() || 
+				!isSelectedListSameWithTheOriginalList();
+	}
+
+	protected boolean isSelectedListSameWithTheOriginalList() {
+		return germplasmList.getId()!=null && originalGermplasmList!=null 
+		&& germplasmList.getId() == originalGermplasmList.getId();
+	}
+
+	protected boolean isSelectedListAnExistingList() {
+		return germplasmList.getType()!=null && !FOLDER_TYPE.equalsIgnoreCase(germplasmList.getType())
+				&& (germplasmList.getId()!=null && originalGermplasmList==null);
+	}
+
+	protected boolean isSelectedListLocked() {
+		return germplasmList!=null && germplasmList.getStatus()>=100;
+	}
+
+	private boolean isListDateValid(ListDateField listDateField) {
+		
+		try {
+			listDateField.validate();
+		} catch (InvalidValueException e) {
+			LOG.error(e.getMessage(),e);
+			MessageNotifier.showRequiredFieldError(getWindow().getParent().getWindow(), e.getMessage());
+			return false;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * Parse the date value return from a DateField object to this format yyyymmdd
+	 * @param listDate string with format: E MMM dd HH:mm:ss Z yyyy
+	 * 		  If doesn't follow the format, will return the current date
+	 * @return
+	 */
+	protected Long getCurrentParsedListDate(String listDate) {
+		Date date;
+		try {
+			date = new SimpleDateFormat("E MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH).parse(listDate);
+		} catch (ParseException e) {
+			date = new Date();
+			LOG.error(e.getMessage(),e);
+		}
+		
+		SimpleDateFormat formatter = new SimpleDateFormat(DateUtil.DATE_AS_NUMBER_FORMAT);
+		
+		return Long.parseLong(formatter.format(date));
 	}
 }
---
+++ src/test/java/org/generationcp/breeding/manager/customcomponent/SaveListAsDialogTest.java
@@ -0,0 +1,119 @@
+package org.generationcp.breeding.manager.customcomponent;
+
+import java.util.Calendar;
+
+import junit.framework.Assert;
+
+import org.generationcp.middleware.pojos.GermplasmList;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class SaveListAsDialogTest {
+	
+	private static SaveListAsDialog dialog;
+	private static GermplasmList germplasmList;
+	private static GermplasmList originalGermplasmList;
+	private static SaveListAsDialogSource source; 
+	
+	@Before
+	public void setUp(){
+		createGermplasmList();
+		source = Mockito.mock(SaveListAsDialogSource.class);
+		dialog = new SaveListAsDialog(source, germplasmList);
+	}
+	
+	private void createGermplasmList() {
+		germplasmList = new GermplasmList();
+		germplasmList.setName("Test List Name");
+		germplasmList.setDescription("Test Description");
+		germplasmList.setType("LST");
+		germplasmList.setDate(Long.parseLong("20141105"));
+		germplasmList.setNotes("Sample Notes");
+	}
+
+	@Test
+	public void testGetCurrentParsedListDateForValidDateFormat(){
+		Long expectedDate = 20111106L;
+		Long parsedDate = dialog.getCurrentParsedListDate("Thu Nov 06 09:39:00 SGT 2011");
+		
+		Assert.assertEquals("Expected for input E MMM dd HH:mm:ss Z yyyy will return yyyymmdd but didn't.", expectedDate, parsedDate);
+	}
+	
+	@Test
+	public void testGetCurrentParsedListDateForInvalidDateFormat(){
+		Calendar currentDate = Calendar.getInstance();
+		String currentDateString = String.valueOf(currentDate.get(Calendar.YEAR))
+				+ appendZeroForSingleDigitMonthOrDay(currentDate.get(Calendar.MONTH) + 1) 
+				+ appendZeroForSingleDigitMonthOrDay(currentDate.get(Calendar.DAY_OF_MONTH));
+		Long expectedDate = Long.parseLong(currentDateString);
+		
+		//invalid date
+		Long parsedDate = dialog.getCurrentParsedListDate("2014-22-22");
+		Assert.assertEquals("Expected for invalid input return the current date in this format yyyymmdd but didn't.", expectedDate, parsedDate);
+	}
+	
+	private String appendZeroForSingleDigitMonthOrDay(int digit){
+		return (digit < 9)? String.valueOf("0" + digit) : String.valueOf(digit);
+	}
+	
+	@Test
+	public void testIsSelectedListLockedReturnsTrueForAList(){
+		germplasmList.setStatus(100);
+		Assert.assertTrue("Expected to return true for a germplasm list with status >= 100 but didn't.", dialog.isSelectedListLocked());
+	}
+	
+	@Test
+	public void testIsSelectedListLockedReturnsFalseForAList(){
+		germplasmList.setStatus(1);
+		Assert.assertFalse("Expected to return false for a germplasm list with status < 100 but didn't.",dialog.isSelectedListLocked());
+		
+		//reset germplasm list instance
+		createGermplasmList();
+		
+		germplasmList = null;
+		Assert.assertFalse("Expected to return false for a germplasm list that is null but didn't.", dialog.isSelectedListLocked());
+	}
+	
+	@Test
+	public void testIsSelectedListAnExistingListReturnsTrueForAList(){
+		germplasmList.setId(-1);
+		germplasmList.setType("LST");
+		dialog.setOriginalGermplasmList(null);
+		
+		Assert.assertTrue("Expected to return true for a germplasm list with LST type but didn't.", dialog.isSelectedListAnExistingList());
+	}
+	
+	@Test
+	public void testIsSelectedListAnExistingListReturnsFalseForAList(){
+		//new list
+		germplasmList.setId(null);
+		germplasmList.setType("LST");
+		dialog.setOriginalGermplasmList(null);
+		
+		Assert.assertFalse("Expected to return false for a germplasm list with id = null but didn't.", dialog.isSelectedListAnExistingList());
+		
+		//is a folder
+		germplasmList.setId(-1);
+		germplasmList.setType("FOLDER");
+		dialog.setOriginalGermplasmList(null);
+		Assert.assertFalse("Expected to return false when the item selected is a folder but didn't.", dialog.isSelectedListAnExistingList());
+	}
+	
+	@Test
+	public void testIsSelectedListSameWithTheOriginalListReturnsTrueForAList(){
+		germplasmList.setId(-1);
+		dialog.setOriginalGermplasmList(germplasmList);
+		
+		Assert.assertTrue("Expecting the selected list is the original list in the save dialog but didn't.",dialog.isSelectedListSameWithTheOriginalList());
+	}
+	
+	@Test
+	public void testIsSelectedListSameWithTheOriginalListReturnsFalseForAList(){
+		germplasmList.setId(-1);
+		originalGermplasmList = dialog.getOriginalGermplasmList();
+		originalGermplasmList.setId(-2);
+		
+		Assert.assertTrue("Expecting the selected list is not the original list in the save dialog but didn't.",dialog.isSelectedListSameWithTheOriginalList());
+	}
+}
