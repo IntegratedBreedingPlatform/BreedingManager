550db4a4d4341efec545387499b8afc6f9b57395
Darla Ani-Hermo <darla@efficio.us.com>
2015-03-27 13:38:05 +0800

    Remove deprecated classes in List Manager
    BMS-529
    
    Reviewer: Crisjaira Villarino

--- src/main/java/org/generationcp/breeding/manager/listmanager/dialog/SelectLocationFolderDialogSource.java
+++
@@ -1,7 +0,0 @@
-package org.generationcp.breeding.manager.listmanager.dialog;
-
-import org.generationcp.middleware.pojos.GermplasmList;
-
-public interface SelectLocationFolderDialogSource {
-	public void setSelectedFolder(GermplasmList folder);
-}
