c05500cd22525e0c4e5d9e026f3d3c4311876f55
Crisjaira Villarino <crisjaira.villarino@leafnode.io>
2015-08-13 09:47:16 +0800

    Created test for getEvenEntries and getOddEntries methods
    BMS-1347
    
    Reviewer: Marc Ulysis De Ramos

--- src/test/java/org/generationcp/breeding/manager/crossingmanager/util/test/CrossingManagerUtilTest.java
+++ src/test/java/org/generationcp/breeding/manager/crossingmanager/util/test/CrossingManagerUtilTest.java
@@ -1,17 +1,21 @@
 
 package org.generationcp.breeding.manager.crossingmanager.util.test;
 
+import java.util.Collection;
+
 import junit.framework.Assert;
 
 import org.generationcp.breeding.manager.crossingmanager.util.CrossingManagerUtil;
+import org.generationcp.commons.constant.ColumnLabels;
 import org.generationcp.middleware.pojos.Germplasm;
 import org.junit.Test;
 
+import com.vaadin.data.Item;
+import com.vaadin.ui.Table;
+
 public class CrossingManagerUtilTest {
 
-	private CrossingManagerUtilTest() {
-		// not implemented
-	}
+	private static final int NO_OF_ENTRIES = 15;
 
 	@Test
 	public void testSetCrossingBreedingMethod() {
@@ -63,4 +67,41 @@ public class CrossingManagerUtilTest {
 		Assert.assertEquals(Integer.valueOf(106), gc.getMethodId());
 	}
 
+	@Test
+	public void testGetOddEntries() {
+		Table listDataTable = this.getListDataTable();
+
+		Collection<?> itemsWithOddEntryNo = CrossingManagerUtil.getOddEntries(listDataTable);
+
+		for (Object item : itemsWithOddEntryNo) {
+			Integer entryNo = (Integer) listDataTable.getItem(item).getItemProperty(ColumnLabels.ENTRY_ID.getName()).getValue();
+			Assert.assertTrue("Expecting that the row has odd entry no but didn't.", entryNo % 2 != 0);
+		}
+	}
+
+	@Test
+	public void testGetEvenEntries() {
+		Table listDataTable = this.getListDataTable();
+
+		Collection<?> itemsWithEvenEntryNo = CrossingManagerUtil.getEvenEntries(listDataTable);
+
+		for (Object item : itemsWithEvenEntryNo) {
+			Integer entryNo = (Integer) listDataTable.getItem(item).getItemProperty(ColumnLabels.ENTRY_ID.getName()).getValue();
+			Assert.assertTrue("Expecting that the row has even entry no but didn't.", entryNo % 2 == 0);
+		}
+	}
+
+	private Table getListDataTable() {
+		Table listDataTable = new Table();
+
+		listDataTable.addContainerProperty(ColumnLabels.ENTRY_ID.getName(), Integer.class, null);
+		listDataTable.setColumnHeader(ColumnLabels.ENTRY_ID.getName(), "#");
+
+		for (int i = 1; i <= NO_OF_ENTRIES; i++) {
+			Item newItem = listDataTable.getContainerDataSource().addItem(i);
+			newItem.getItemProperty(ColumnLabels.ENTRY_ID.getName()).setValue(i);
+		}
+
+		return listDataTable;
+	}
 }
