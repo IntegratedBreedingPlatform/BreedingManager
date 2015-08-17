d7ce537ef94f2350bdc93b47333a07c76629acec
Cyrus Venn Casada <cvpcasada@outlook.com>
2015-08-17 11:13:05 +0800

    Cleanup unused imports

--- src/test/java/org/generationcp/breeding/manager/listimport/GermplasmListTemplateDownloaderTest.java
+++ src/test/java/org/generationcp/breeding/manager/listimport/GermplasmListTemplateDownloaderTest.java
@@ -1,22 +1,13 @@
 package org.generationcp.breeding.manager.listimport;
 
 import java.io.File;
-import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 
 import com.vaadin.Application;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.Window;
-import junit.framework.Assert;
-import org.apache.commons.lang.StringUtils;
-import org.generationcp.commons.spring.util.ContextUtil;
 import org.generationcp.commons.util.FileDownloadResource;
-import org.generationcp.commons.vaadin.spring.SimpleResourceBundleMessageSource;
-import org.generationcp.middleware.exceptions.MiddlewareQueryException;
 import org.generationcp.middleware.manager.api.WorkbenchDataManager;
-import org.generationcp.middleware.pojos.workbench.CropType;
-import org.generationcp.middleware.pojos.workbench.Project;
-import org.generationcp.middleware.pojos.workbench.WorkbenchSetting;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
