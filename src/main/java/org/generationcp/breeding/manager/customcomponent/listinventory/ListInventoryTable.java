89e20baf6e992324a5a50dc8daa1e06e07aad812
Naymesh <naymesh@leafnode.io>
2015-12-21 14:12:25 +1300

    Placing the Seed Source column in inventory view, to the right of the StockID column for consistency with the main list view.
    Issue: BMS-2079
    Reviewer: MatthewB

--- src/main/java/org/generationcp/breeding/manager/customcomponent/listinventory/ListInventoryTable.java
+++ src/main/java/org/generationcp/breeding/manager/customcomponent/listinventory/ListInventoryTable.java
@@ -103,8 +103,8 @@ public class ListInventoryTable extends TableWithSelectAllLayout implements Init
 		this.listInventoryTable.addContainerProperty(ColumnLabels.NEWLY_RESERVED.getName(), Double.class, null);
 		this.listInventoryTable.addContainerProperty(ColumnLabels.COMMENT.getName(), String.class, null);
 		this.listInventoryTable.addContainerProperty(ColumnLabels.STOCKID.getName(), Label.class, null);
-		this.listInventoryTable.addContainerProperty(ColumnLabels.LOT_ID.getName(), Integer.class, null);
 		this.listInventoryTable.addContainerProperty(ColumnLabels.SEED_SOURCE.getName(), String.class, null);
+		this.listInventoryTable.addContainerProperty(ColumnLabels.LOT_ID.getName(), Integer.class, null);
 
 		this.listInventoryTable.setColumnHeader(ColumnLabels.TAG.getName(), this.messageSource.getMessage(Message.CHECK_ICON));
 		this.listInventoryTable.setColumnHeader(ColumnLabels.ENTRY_ID.getName(), this.messageSource.getMessage(Message.HASHTAG));
@@ -126,10 +126,10 @@ public class ListInventoryTable extends TableWithSelectAllLayout implements Init
 				ColumnLabels.COMMENT.getTermNameFromOntology(this.ontologyDataManager));
 		this.listInventoryTable.setColumnHeader(ColumnLabels.STOCKID.getName(),
 				ColumnLabels.STOCKID.getTermNameFromOntology(this.ontologyDataManager));
-		this.listInventoryTable.setColumnHeader(ColumnLabels.LOT_ID.getName(),
-				ColumnLabels.LOT_ID.getTermNameFromOntology(this.ontologyDataManager));
 		this.listInventoryTable.setColumnHeader(ColumnLabels.SEED_SOURCE.getName(),
 				ColumnLabels.SEED_SOURCE.getTermNameFromOntology(this.ontologyDataManager));
+		this.listInventoryTable.setColumnHeader(ColumnLabels.LOT_ID.getName(),
+				ColumnLabels.LOT_ID.getTermNameFromOntology(this.ontologyDataManager));
 	}
 
 	public void loadInventoryData() {
