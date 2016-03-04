e348a90a3838538d84ea2f9714d5c66f8f7b6a05
Alejandro Mildiner <amildiner@droptek.com>
2016-03-03 23:27:49 -0300

    Rename class

--- src/main/java/org/generationcp/breeding/manager/listimport/actions/GermplasmBuilder.java
+++ src/main/java/org/generationcp/breeding/manager/listimport/actions/GermplasmBuilder.java
@@ -2,9 +2,26 @@ package org.generationcp.breeding.manager.listimport.actions;
 
 import org.generationcp.middleware.pojos.Germplasm;
 
+public class GermplasmBuilder implements Builder<Germplasm,GermplasmDataProvider> {
 
-public interface GermplasmBuilder {
+	@Override
+	public Germplasm build(GermplasmDataProvider provider){
+		Germplasm germplasm = new Germplasm();
 
-	public Germplasm build(GermplasmDataProvider provider);
+		germplasm.setGid(provider.getGID());
+		germplasm.setGnpgs(provider.getProgenitors());
+		germplasm.setGpid1(provider.getGPID1());
+		germplasm.setGnpgs(provider.getGPID2());
+		germplasm.setUserId(provider.getDateValue());
+		germplasm.setUserId(provider.getUserId());
+		germplasm.setLocationId(provider.getLocationId());
+		germplasm.setGdate(provider.getDateValue());
+		germplasm.setMethodId(provider.getMethodId());
+		germplasm.setLgid(provider.getLgid());
+		germplasm.setGrplce(provider.getGrplce());
+		germplasm.setReferenceId(provider.getReferenceId());
+		germplasm.setMgid(provider.getMgid());
 
+		return germplasm;
+	}
 }
