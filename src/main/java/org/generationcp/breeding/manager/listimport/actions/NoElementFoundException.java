62fb17942859d296429541ef1c96de5c9a55fe8a
Alejandro Mildiner <amildiner@droptek.com>
2016-03-06 20:49:32 -0300

    add exception

---
+++ src/main/java/org/generationcp/breeding/manager/listimport/actions/NoElementFoundException.java
@@ -0,0 +1,13 @@
+package org.generationcp.breeding.manager.listimport.actions;
+
+
+public class NoElementFoundException extends Exception {
+
+	public NoElementFoundException() {
+
+	}
+	public NoElementFoundException(String message) {
+		super(message);
+	}
+
+}
