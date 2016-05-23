62750ea4a6926995725ea13a9685f138152b92ea
Naymesh <naymesh@leafnode.io>
2016-05-24 10:59:57 +1200

    Applied auto source cleanup and formatting.
    Issue: BMS-2722
    Reviewer: AkhilM

--- src/test/java/org/generationcp/breeding/manager/customcomponent/PagedTableWithSelectAllLayoutTest.java
+++ src/test/java/org/generationcp/breeding/manager/customcomponent/PagedTableWithSelectAllLayoutTest.java
@@ -1,183 +1,182 @@
+
 package org.generationcp.breeding.manager.customcomponent;
 
-import com.vaadin.ui.CheckBox;
-import junit.framework.Assert;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.generationcp.breeding.manager.customfields.PagedBreedingManagerTable;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.vaadin.ui.CheckBox;
+
+import junit.framework.Assert;
 
 /**
  * Created by Aldrin Batac on 5/23/16.
  */
 public class PagedTableWithSelectAllLayoutTest {
 
+	private static final String CHECKBOX_COLUMN_ID = "CheckBoxColumnId";
 
-    private static final String CHECKBOX_COLUMN_ID = "CheckBoxColumnId";
+	private final PagedTableWithSelectAllLayout pagedTableWithSelectAllLayout =
+			new PagedTableWithSelectAllLayout(0, PagedTableWithSelectAllLayoutTest.CHECKBOX_COLUMN_ID);
 
-    private PagedTableWithSelectAllLayout pagedTableWithSelectAllLayout = new PagedTableWithSelectAllLayout(0, CHECKBOX_COLUMN_ID);
+	@Before
+	public void init() {
+		this.pagedTableWithSelectAllLayout.instantiateComponents();
+		this.initializePagedBreedingManagerTable();
+	}
 
-    @Before
-    public void init(){
+	@Test
+	public void testGetAllEntriesPerPage() {
 
-        pagedTableWithSelectAllLayout.instantiateComponents();
+		final int pageLength = 5;
+		final int firstPage = 1;
+		final int lastPage = 21;
 
-        this.initializePagedBreedingManagerTable();
+		final List<Object> entriesList = this.createListObject();
+		this.pagedTableWithSelectAllLayout.getTable().setPageLength(pageLength);
 
-    }
+		final List<Object> result = this.pagedTableWithSelectAllLayout.getAllEntriesPerPage(entriesList, firstPage);
 
+		Assert.assertEquals("The number of entries per page should be equal to the table's page length.", pageLength, result.size());
 
-    @Test
-    public void testGetAllEntriesPerPage(){
+		final List<Object> result2 = this.pagedTableWithSelectAllLayout.getAllEntriesPerPage(entriesList, lastPage);
 
+		Assert.assertEquals("The last page should only have 1 item", 1, result2.size());
 
-        int pageLength = 5;
-        int firstPage = 1;
-        int lastPage = 21;
+	}
 
-        List<Object> entriesList = createListObject();
-        pagedTableWithSelectAllLayout.getTable().setPageLength(pageLength);
+	@Test
+	public void testUpdateTagPerRowItem() {
 
-        List<Object> result = pagedTableWithSelectAllLayout.getAllEntriesPerPage(entriesList, firstPage);
+		final Integer firstSelectedObjectItemId = 1;
+		final Integer secondSelectedObjectItemId = 2;
 
-        Assert.assertEquals("The number of entries per page should be equal to the table's page length.", pageLength, result.size());
+		final List<Object> selectedItems = new ArrayList<>();
+		selectedItems.add(firstSelectedObjectItemId);
+		selectedItems.add(secondSelectedObjectItemId);
 
-        List<Object> result2 = pagedTableWithSelectAllLayout.getAllEntriesPerPage(entriesList, lastPage);
+		final List<Object> loadedItems = new ArrayList<>(this.pagedTableWithSelectAllLayout.getTable().getItemIds());
 
-        Assert.assertEquals("The last page should only have 1 item", 1, result2.size());
+		this.pagedTableWithSelectAllLayout.updateTagPerRowItem(selectedItems, loadedItems);
 
-    }
+		final PagedBreedingManagerTable table = this.pagedTableWithSelectAllLayout.getTable();
 
-    @Test
-    public void testUpdateTagPerRowItem() {
+		final CheckBox checkboxOfFirstItem = (CheckBox) table.getItem(firstSelectedObjectItemId)
+				.getItemProperty(PagedTableWithSelectAllLayoutTest.CHECKBOX_COLUMN_ID).getValue();
+		Assert.assertTrue("The first item in table is selected so the checkbox value should be true",
+				(Boolean) checkboxOfFirstItem.getValue());
 
-        Integer firstSelectedObjectItemId = 1;
-        Integer secondSelectedObjectItemId = 2;
+		final CheckBox checkboxOfSecondItem = (CheckBox) table.getItem(secondSelectedObjectItemId)
+				.getItemProperty(PagedTableWithSelectAllLayoutTest.CHECKBOX_COLUMN_ID).getValue();
+		Assert.assertTrue("The second item in table is selected so the checkbox value should be true",
+				(Boolean) checkboxOfSecondItem.getValue());
 
-        List<Object> selectedItems = new ArrayList<>();
-        selectedItems.add(firstSelectedObjectItemId);
-        selectedItems.add(secondSelectedObjectItemId);
+		// Other items that are not selected should have a checkbox with false value.
+		for (Integer i = 3; i < table.getItemIds().size(); i++) {
+			final CheckBox checkbox =
+					(CheckBox) table.getItem(i).getItemProperty(PagedTableWithSelectAllLayoutTest.CHECKBOX_COLUMN_ID).getValue();
+			Assert.assertFalse("Item is not selected, the checkbox value should be false.", (Boolean) checkbox.getValue());
+		}
 
-        List<Object> loadedItems = new ArrayList<>(pagedTableWithSelectAllLayout.getTable().getItemIds());
+	}
 
-        pagedTableWithSelectAllLayout.updateTagPerRowItem(selectedItems, loadedItems);
+	@Test
+	public void testUpdateSelectAllCheckBoxStatusEmptyList() {
 
-        PagedBreedingManagerTable table = pagedTableWithSelectAllLayout.getTable();
+		// Set the select all checkbox to true so we can verify that it's changed later
+		this.pagedTableWithSelectAllLayout.getSelectAllCheckBox().setValue(true);
 
-        CheckBox checkboxOfFirstItem = (CheckBox) table.getItem(firstSelectedObjectItemId).getItemProperty(CHECKBOX_COLUMN_ID).getValue();
-        Assert.assertTrue("The first item in table is selected so the checkbox value should be true", (Boolean) checkboxOfFirstItem.getValue());
+		this.pagedTableWithSelectAllLayout.updateSelectAllCheckBoxStatus(new ArrayList<>());
 
-        CheckBox checkboxOfSecondItem = (CheckBox) table.getItem(secondSelectedObjectItemId).getItemProperty(CHECKBOX_COLUMN_ID).getValue();
-        Assert.assertTrue("The second item in table is selected so the checkbox value should be true", (Boolean) checkboxOfSecondItem.getValue());
+		Assert.assertFalse("There entry list is empty so the select all checkbox value should be false",
+				(Boolean) this.pagedTableWithSelectAllLayout.getSelectAllCheckBox().getValue());
 
-        // Other items that are not selected should have a checkbox with false value.
-        for (Integer i = 3; i < table.getItemIds().size(); i++) {
-            CheckBox checkbox = (CheckBox) table.getItem(i).getItemProperty(CHECKBOX_COLUMN_ID).getValue();
-            Assert.assertFalse("Item is not selected, the checkbox value should be false.", (Boolean) checkbox.getValue());
-        }
+	}
 
+	@Test
+	public void testUpdateSelectAllCheckBoxStatusAllEntriesInAPageAreSelected() {
 
-    }
+		this.pagedTableWithSelectAllLayout.getTable().setCurrentPage(1);
 
-    @Test
-    public void testUpdateSelectAllCheckBoxStatusEmptyList() {
+		final List<Object> entriesList = this.createEntriesList();
 
-        // Set the select all checkbox to true so we can verify that it's changed later
-        pagedTableWithSelectAllLayout.getSelectAllCheckBox().setValue(true);
+		this.markCheckboxItemsInTable(entriesList, true);
 
-        pagedTableWithSelectAllLayout.updateSelectAllCheckBoxStatus(new ArrayList<>());
+		this.pagedTableWithSelectAllLayout.updateSelectAllCheckBoxStatus(entriesList);
 
-        Assert.assertFalse("There entry list is empty so the select all checkbox value should be false", (Boolean) pagedTableWithSelectAllLayout.getSelectAllCheckBox().getValue());
+		Assert.assertTrue("All entries in page are selected, the select all checkbox value should be true",
+				(Boolean) this.pagedTableWithSelectAllLayout.getSelectAllCheckBox().getValue());
 
-    }
+	}
 
-    @Test
-    public void testUpdateSelectAllCheckBoxStatusAllEntriesInAPageAreSelected() {
+	@Test
+	public void testUpdateSelectAllCheckBoxStatusSomeEntriesInAPageAreSelected() {
 
+		this.pagedTableWithSelectAllLayout.getTable().setCurrentPage(1);
 
-        pagedTableWithSelectAllLayout.getTable().setCurrentPage(1);
+		final List<Object> entriesList = this.createEntriesList();
 
-        List<Object> entriesList = this.createEntriesList();
+		// Mark the first item as selected
+		final PagedBreedingManagerTable table = this.pagedTableWithSelectAllLayout.getTable();
+		final CheckBox cb = (CheckBox) table.getItem(1).getItemProperty(PagedTableWithSelectAllLayoutTest.CHECKBOX_COLUMN_ID).getValue();
+		cb.setValue(true);
 
-        markCheckboxItemsInTable(entriesList, true);
+		this.pagedTableWithSelectAllLayout.updateSelectAllCheckBoxStatus(entriesList);
 
-        pagedTableWithSelectAllLayout.updateSelectAllCheckBoxStatus(entriesList);
+		Assert.assertFalse("Some entries in page are selected, the select all checkbox value should be false",
+				(Boolean) this.pagedTableWithSelectAllLayout.getSelectAllCheckBox().getValue());
 
-        Assert.assertTrue("All entries in page are selected, the select all checkbox value should be true", (Boolean) pagedTableWithSelectAllLayout.getSelectAllCheckBox().getValue());
+	}
 
-    }
+	private List<Object> createEntriesList() {
 
-    @Test
-    public void testUpdateSelectAllCheckBoxStatusSomeEntriesInAPageAreSelected() {
+		final List<Object> entriesList = new ArrayList<>();
+		entriesList.add(1);
+		entriesList.add(2);
+		entriesList.add(3);
+		entriesList.add(4);
+		entriesList.add(5);
+		return entriesList;
+	}
 
+	private void markCheckboxItemsInTable(final List<Object> selectedItems, final Boolean checkboxValue) {
 
-        pagedTableWithSelectAllLayout.getTable().setCurrentPage(1);
+		final PagedBreedingManagerTable table = this.pagedTableWithSelectAllLayout.getTable();
 
-        List<Object> entriesList = this.createEntriesList();
+		for (final Object itemId : selectedItems) {
+			final CheckBox cb =
+					(CheckBox) table.getItem(itemId).getItemProperty(PagedTableWithSelectAllLayoutTest.CHECKBOX_COLUMN_ID).getValue();
+			cb.setValue(checkboxValue);
+		}
 
-        // Mark the first item as selected
-        PagedBreedingManagerTable table = pagedTableWithSelectAllLayout.getTable();
-        CheckBox cb = (CheckBox) table.getItem(1).getItemProperty(CHECKBOX_COLUMN_ID).getValue();
-        cb.setValue(true);
+	}
 
-        pagedTableWithSelectAllLayout.updateSelectAllCheckBoxStatus(entriesList);
+	void initializePagedBreedingManagerTable() {
 
-        Assert.assertFalse("Some entries in page are selected, the select all checkbox value should be false", (Boolean) pagedTableWithSelectAllLayout.getSelectAllCheckBox().getValue());
+		final PagedBreedingManagerTable table = this.pagedTableWithSelectAllLayout.getTable();
 
-    }
+		table.addContainerProperty(PagedTableWithSelectAllLayoutTest.CHECKBOX_COLUMN_ID, CheckBox.class, null);
 
+		int i = 1;
+		while (i <= 101) {
+			table.addItem(i);
+			table.getItem(i).getItemProperty(PagedTableWithSelectAllLayoutTest.CHECKBOX_COLUMN_ID).setValue(new CheckBox());
+			i++;
+		}
 
-    private List<Object> createEntriesList() {
+	}
 
-        List<Object> entriesList = new ArrayList<>();
-        entriesList.add(1);
-        entriesList.add(2);
-        entriesList.add(3);
-        entriesList.add(4);
-        entriesList.add(5);
-
-        return entriesList;
-
-    }
-
-    private void markCheckboxItemsInTable(List<Object> selectedItems, Boolean checkboxValue) {
-
-        PagedBreedingManagerTable table = pagedTableWithSelectAllLayout.getTable();
-
-        for (Object itemId : selectedItems) {
-            CheckBox cb = (CheckBox) table.getItem(itemId).getItemProperty(CHECKBOX_COLUMN_ID).getValue();
-            cb.setValue(checkboxValue);
-        }
-
-    }
-
-    void initializePagedBreedingManagerTable() {
-
-        PagedBreedingManagerTable table = pagedTableWithSelectAllLayout.getTable();
-
-        table.addContainerProperty(CHECKBOX_COLUMN_ID, CheckBox.class, null);
-
-        int i = 1;
-        while (i <= 101){
-            table.addItem(i);
-            table.getItem(i).getItemProperty(CHECKBOX_COLUMN_ID).setValue(new CheckBox());
-            i++;
-        }
-
-    }
-
-    List<Object> createListObject(){
-        List<Object> entriesList = new ArrayList<>();
-        int i = 1;
-        while (i <= 101){
-            entriesList.add(new Object());
-            i++;
-        }
-        return entriesList;
-    }
-
-
+	List<Object> createListObject() {
+		final List<Object> entriesList = new ArrayList<>();
+		int i = 1;
+		while (i <= 101) {
+			entriesList.add(new Object());
+			i++;
+		}
+		return entriesList;
+	}
 
 }
