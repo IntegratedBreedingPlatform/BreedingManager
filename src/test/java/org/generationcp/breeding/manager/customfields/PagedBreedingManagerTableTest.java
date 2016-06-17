773d463f2f700b58c0a60faf0dbd6f93a11127ee
Mae Isabelle Turiana <maeisabelle@gmail.com>
2016-06-17 17:28:24 +0800

    Applied formatting and cleanup
    BMS-2796

--- src/test/java/org/generationcp/breeding/manager/customfields/PagedBreedingManagerTableTest.java
+++ src/test/java/org/generationcp/breeding/manager/customfields/PagedBreedingManagerTableTest.java
@@ -1,18 +1,14 @@
 
 package org.generationcp.breeding.manager.customfields;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.annotation.Resource;
 
 import junit.framework.Assert;
 
 import org.generationcp.breeding.manager.data.initializer.GermplasmQueryFactoryTestDataInitializer;
 import org.generationcp.breeding.manager.listmanager.ListManagerMain;
 import org.generationcp.commons.constant.ColumnLabels;
-import org.generationcp.middleware.manager.api.GermplasmDataManager;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -47,14 +43,12 @@ public class PagedBreedingManagerTableTest {
 	private ListManagerMain listManagerMain;
 
 	private GermplasmQueryFactoryTestDataInitializer germplasmQueryFactoryTDI;
-	
-	@Resource
-	private GermplasmDataManager germplasmDataManager;
 
 	@Before
 	public void setUp() {
-		this.pagedTable = new PagedBreedingManagerTable(PagedBreedingManagerTableTest.INIT_RECORD_COUNT,
-				PagedBreedingManagerTableTest.MAX_RECORD_COUNT);
+		this.pagedTable =
+				new PagedBreedingManagerTable(PagedBreedingManagerTableTest.INIT_RECORD_COUNT,
+						PagedBreedingManagerTableTest.MAX_RECORD_COUNT);
 		// we need to be able to stub the handler call so we can use mockito's verify methods against it
 		this.tableHandler = Mockito.mock(TableMultipleSelectionHandler.class);
 		this.pagedTable.setTableHandler(this.tableHandler);
@@ -127,7 +121,7 @@ public class PagedBreedingManagerTableTest {
 	@Test
 	public void testUpdateBatchSizeBatchSizeNotUpdated() {
 		this.setupContainerDataSource(0);
-		
+
 		final int batchSize = this.pagedTable.getBatchSize();
 
 		final int newPageLength = this.pagedTable.getPageLength() + 5;
@@ -139,28 +133,25 @@ public class PagedBreedingManagerTableTest {
 		Assert.assertEquals("The batch size should be equal to the updated batch size", batchSize, updatedBatchSize);
 		Assert.assertFalse("The new page length and the updated batch size should not be equal", newPageLength == updatedBatchSize);
 	}
-	
-	private void setupContainerDataSource(int numberOfItems) {
-		final QueryDefinition queryDefinition = new LazyQueryDefinition(false, BATCH_SIZE);
+
+	private void setupContainerDataSource(final int numberOfItems) {
+		final QueryDefinition queryDefinition = new LazyQueryDefinition(false, PagedBreedingManagerTableTest.BATCH_SIZE);
 		final QueryView queryView = Mockito.mock(QueryView.class);
 		Mockito.doReturn(numberOfItems).when(queryView).size();
 		Mockito.doReturn(queryDefinition).when(queryView).getQueryDefinition();
-		
+
 		final LazyQueryContainer container = new LazyQueryContainer(queryView);
-		if(numberOfItems > 0) {
-			for (int i = 0; i < numberOfItems ; i++) {
+		if (numberOfItems > 0) {
+			for (int i = 0; i < numberOfItems; i++) {
 				Mockito.doReturn(i).when(queryView).addItem();
 			}
-			
+
 			for (int i = 0; i < numberOfItems; i++) {
 				container.addItem();
 			}
 		}
-		
+
 		this.pagedTable.setContainerDataSource(container);
-		
-		
-		
-		
+
 	}
 }
