6c426461c960add89cd87a14930816a76d748e19
abhishekptl <abhishek@leafnode.io>
2016-09-06 10:06:51 +0530

    Creating utilty method to do manually logout
    This manually logout utility will set authentication to null and delete all cookies from the context path of application.
    
    Cookies needs to delete because PreAuthentication mechanism try to resolve pre-authenticated user from session,cookie and then database(workbench_runtime_data).
    Singing out from Workbench will clear session, authentication to null and clear cookies for all downstream applications.
    
    issue: BMS-3194
    reviewer: Sunny

--- src/main/java/org/generationcp/breeding/manager/application/LogoutServlet.java
+++ src/main/java/org/generationcp/breeding/manager/application/LogoutServlet.java
@@ -1,34 +1,18 @@
 package org.generationcp.breeding.manager.application;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.generationcp.commons.util.LogoutUtil;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession context = request.getSession(false);
-		if(context != null) {
-			context.invalidate();
-		}
-
-		SecurityContext context1 = SecurityContextHolder.getContext();
-		context1.setAuthentication((Authentication)null);
-
-		for(Cookie cookie : request.getCookies()) {
-			cookie.setMaxAge(0);
-		}
-
-		SecurityContextHolder.clearContext();
+		LogoutUtil.manuallyLogout(request, response);
 		response.getOutputStream().print("BreedingManager successfully logged out");
 	}
 }
