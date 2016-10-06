6c34edf0b5f419a8332c68b8eecfc02205e3156c
Marc Ulysis De Ramos <marc.ramos@leafnode.io>
2016-10-06 13:31:54 +0800

    Added test for the method initializeGuideMessage()
    BMS-3383

--- src/test/java/org/generationcp/breeding/manager/listimport/SelectGermplasmWindowTest.java
+++ src/test/java/org/generationcp/breeding/manager/listimport/SelectGermplasmWindowTest.java
@@ -1,8 +1,10 @@
 
 package org.generationcp.breeding.manager.listimport;
 
+import org.generationcp.breeding.manager.application.Message;
 import org.generationcp.breeding.manager.listimport.actions.ProcessImportedGermplasmAction;
 import org.generationcp.commons.constant.ColumnLabels;
+import org.generationcp.commons.vaadin.spring.SimpleResourceBundleMessageSource;
 import org.generationcp.middleware.domain.oms.Term;
 import org.generationcp.middleware.domain.oms.TermId;
 import org.generationcp.middleware.exceptions.MiddlewareQueryException;
@@ -16,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.Window;
 
@@ -24,14 +27,20 @@ public class SelectGermplasmWindowTest {
 
 	@Mock
 	private OntologyDataManager ontologyDataManager;
+	
 	@Mock
 	private ProcessImportedGermplasmAction source;
+	
 	@Mock
 	private Window parentWindow;
+	
+	@Mock
+	private SimpleResourceBundleMessageSource messageSource;
 
 	private final String germplasmName = "Germplasm Name";
 	private final int index = 2;
 	private Germplasm germplasm;
+	private final Integer noOfImportedGemplasm = 1;
 
 	private SelectGermplasmWindow selectGermplasmWindow;
 
@@ -41,8 +50,10 @@ public class SelectGermplasmWindowTest {
 		this.germplasm.setGid(1);
 
 		this.selectGermplasmWindow =
-				new SelectGermplasmWindow(this.source, this.germplasmName, this.index, this.germplasm, this.parentWindow);
+				new SelectGermplasmWindow(this.source, this.germplasmName, this.index, this.germplasm, this.parentWindow, this.noOfImportedGemplasm);
 		this.selectGermplasmWindow.setOntologyDataManager(this.ontologyDataManager);
+		this.selectGermplasmWindow.setMessageSource(this.messageSource);
+		
 	}
 
 	@Test
@@ -90,5 +101,19 @@ public class SelectGermplasmWindowTest {
 		Assert.assertEquals("Ontology Name", table.getColumnHeader(ColumnLabels.GERMPLASM_LOCATION.getName()));
 		Assert.assertEquals("Ontology Name", table.getColumnHeader(ColumnLabels.BREEDING_METHOD_NAME.getName()));
 		Assert.assertEquals("Ontology Name", table.getColumnHeader(ColumnLabels.PARENTAGE.getName()));
+	}
+	
+	@Test
+	public void testInitializeGuideMessage() {
+		//Initialize the select Germplasm Label
+		final Label selectGermplasmLabel = new Label("", Label.CONTENT_XHTML);
+		this.selectGermplasmWindow.setSelectGermplasmLabel(selectGermplasmLabel);
+		
+		Mockito.when(this.messageSource.getMessage(Message.GERMPLASM_MATCHES_LABEL,
+				new Object[] {this.index + 1, this.noOfImportedGemplasm, this.germplasmName})).thenReturn(Message.GERMPLASM_MATCHES_LABEL.toString());	
+		
+		this.selectGermplasmWindow.initializeGuideMessage();
+		
+		Assert.assertEquals("The selec germplasm label's value should be " + Message.GERMPLASM_MATCHES_LABEL.toString(), Message.GERMPLASM_MATCHES_LABEL.toString(), selectGermplasmLabel.getValue());
 	}
 }
