f5e689c528e0fc61cd67796a008119a2ba4fc14e
abhishekptl <abhishek@leafnode.io>
2016-12-09 15:42:37 +0530

    Implementation of Close confirmation  dialogue when there is available balance while closing lot
    When there is available balance while closing lots then this dialog display to discard balance and close lot to user. also, apply to all functional will apply the yes/no action to rest lots if multiple lots are selected for closing. Its respective listener to maintain list of this dialog to process one by one.
    
    issue: BMS-3665, BMS-3670
    reviewer: Sunny

---
+++ src/main/java/org/generationcp/breeding/manager/inventory/exception/CloseLotException.java
@@ -0,0 +1,11 @@
+package org.generationcp.breeding.manager.inventory.exception;
+
+public class CloseLotException extends Exception {
+
+	private static final long serialVersionUID = -4976888301866579819L;
+
+	public CloseLotException(String message) {
+		super(message);
+	}
+
+}
