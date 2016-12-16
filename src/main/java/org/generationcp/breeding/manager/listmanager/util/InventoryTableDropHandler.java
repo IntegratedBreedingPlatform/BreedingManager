1d6864d0fe29da03cfea4d4eb767450f990a59fc
chavdahard <hard@leafnode.io>
2016-12-16 17:00:27 +0530

    Added new lot details pop up view's link on Lotid Column of List Builder.
    issue: BMS-3665, BMS-3671
    reviewer: Abhishek

--- src/main/java/org/generationcp/breeding/manager/listmanager/util/InventoryTableDropHandler.java
+++ src/main/java/org/generationcp/breeding/manager/listmanager/util/InventoryTableDropHandler.java
@@ -12,6 +12,7 @@ import org.generationcp.breeding.manager.inventory.ListDataAndLotDetails;
 import org.generationcp.breeding.manager.listmanager.ListComponent;
 import org.generationcp.breeding.manager.listmanager.ListManagerMain;
 import org.generationcp.breeding.manager.listmanager.listeners.GidLinkButtonClickListener;
+import org.generationcp.commons.Listener.LotDetailsButtonClickListener;
 import org.generationcp.commons.constant.ColumnLabels;
 import org.generationcp.commons.vaadin.util.MessageNotifier;
 import org.generationcp.middleware.domain.inventory.ListDataInventory;
@@ -403,7 +404,12 @@ public class InventoryTableDropHandler extends DropHandlerMethods implements Dro
 		newItem.getItemProperty(ColumnLabels.SEED_RESERVATION.getName()).setValue(lotDetail.getCommittedTotalForEntry());
 		newItem.getItemProperty(ColumnLabels.COMMENT.getName()).setValue(lotDetail.getCommentOfLot());
 		newItem.getItemProperty(ColumnLabels.STOCKID.getName()).setValue(lotDetail.getStockIds());
-		newItem.getItemProperty(ColumnLabels.LOT_ID.getName()).setValue(lotDetail.getLotId());
+
+		final Button lotButton = new Button(lotDetail.getLotId().toString(),
+				new LotDetailsButtonClickListener(lotDetail.getEntityIdOfLot(), targetDesignationButton.toString(), this.targetTable, lotDetail.getLotId()));
+		lotButton.setStyleName(BaseTheme.BUTTON_LINK);
+		newItem.getItemProperty(ColumnLabels.LOT_ID.getName()).setValue(lotButton);
+
 		newItem.getItemProperty(ColumnLabels.SEED_SOURCE.getName()).setValue(seedSource);
 		
 
